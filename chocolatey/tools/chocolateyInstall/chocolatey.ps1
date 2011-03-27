c6982c9309df37fbe9fd23714d98f118444e0d8b
Rob Reynolds <ferventcoder@gmail.com>
2011-03-27 04:08:49 -0500

    adding in chocolateyinstall.ps1 script

--- chocolatey/tools/chocolateyInstall/chocolatey.ps1
+++ chocolatey/tools/chocolateyInstall/chocolatey.ps1
@@ -6,6 +6,19 @@ $nugetLibPath = Join-Path $nuGetPath 'lib'
 $nugetChocolateyPath = Join-Path $nuGetPath 'chocolateyInstall'
 $nugetExe = Join-Path $nugetChocolateyPath 'nuget.exe'
 
+function Run-ChocolateyProcess
+{
+  $file, [string]$arguments = $args;
+  $psi = new-object System.Diagnostics.ProcessStartInfo $file;
+  $psi.Arguments = $arguments;
+	#if ($elevated) {
+		$psi.Verb = "runas";
+	#}
+  $psi.WorkingDirectory = get-location;
+  $s = [System.Diagnostics.Process]::Start($psi);
+	$s.WaitForExit(8000);
+}
+
 function Chocolatey-NuGet { 
 param([string] $packageName)
   Write-Host 'Attempting to install ' $packageName ' to ' "$nugetLibPath"
@@ -13,6 +26,7 @@ param([string] $packageName)
   C:\NuGet\chocolateyInstall\NuGet.exe install $packageName /outputdirectory "$nugetLibPath"
   
   Get-ChocolateyBins $packageName
+	Run-ChocolateyPS1 $packageName
 }
 
 function Get-ChocolateyBins {
@@ -32,6 +46,25 @@ param([string] $packageName)
     }
 		Write-Host '================================================'
   }
+}
+
+function Run-ChocolateyPS1 {
+param([string] $packageName)
+  $packageFolder = Get-ChildItem $nugetLibPath | ?{$_.name -match "$packageName*"} | sort name -Descending | select -First 1 
+  if ($packageFolder) { 
+		Write-Host '================================================'
+	Write-Host 'Additional installation - chocolateyinstall.ps1'
+		Write-Host '================================================'
+		Write-Host 'Looking for chocolateyinstall.ps1 in folder: ' $packageFolder.FullName
+		Write-Host 'If chocolateyInstall.ps1 is found, it will be run.'
+		$ps1 = Get-ChildItem  $packageFolder.FullName -recurse | ?{$_.name -match "chocolateyinstall.ps1"} | sort name -Descending | select -First 1
+		write-host "`$ps1 : $ps1" + $ps1.FullName
+		if ($ps1 -notlike '') {
+			$ps1FullPath = $ps1.FullName
+			Write-Host "Running against $ps1FullPath"
+			Run-ChocolateyProcess powershell "$ps1FullPath"
+		}
+	}
 }
 
 function Generate-BinFile {
@@ -64,10 +97,6 @@ function Chocolatey-List {
   C:\NuGet\chocolateyInstall\NuGet.exe list
 }
 
-#default is to install
-if ($args.Length -eq 1 -and $args[0] -notcontains 'list' -and $args[0] -notcontains 'help') {
- Chocolatey-NuGet  $args[0];
-}
 $argsHasInstall = $args -contains 'install'
 if ($argsHasInstall -and $args.Length -eq 2) {
  Chocolatey-NuGet  $args[1];
