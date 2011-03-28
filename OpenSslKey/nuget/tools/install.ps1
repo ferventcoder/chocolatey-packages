2f0a383774c0701537f89c1917e00b97c6d1b657
Rob Reynolds <ferventcoder@gmail.com>
2011-03-27 23:24:26 -0500

    adding opensslkey

---
+++ OpenSslKey/nuget/tools/install.ps1
@@ -0,0 +1,5 @@
+param($installPath, $toolsPath, $package, $project)
+
+Write-Host 'Open SSL Key - Utility to generate XML keys from PEM or DER keys.'
+Write-Host 'Also comes with an exe application. Please install using chocolatey (you can get with install-package chocolatey)'
+Write-Host 'i.e. chocolatey install opensslkey'
