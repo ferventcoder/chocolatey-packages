988b23107932f552a4d3989e2e9e1eb747b97f38
Rob Reynolds <ferventcoder@gmail.com>
2011-03-29 01:53:20 -0500

    updating to catch both versions of windows

--- ie9/tools/chocolateyInstall.ps1
+++ ie9/tools/chocolateyInstall.ps1
@@ -1,11 +1,24 @@
-﻿$url = "http://view.atdmt.com/action/UMIRF_IE_IE9WW_InternationalDL?href=http://download.microsoft.com/download/C/1/6/C167B427-722E-4665-9A40-A37BC5222B0A/IE9-Windows7-x64-enu.exe"
+﻿$os = Get-WmiObject Win32_OperatingSystem
+$winVersionName = $os.Caption
+$winVer = $os.Version
+$bitType = $os.OSArchitecture
+$is64bit = $os.OSArchitecture -eq "64-bit"
+
+#auto installers
+#$urlToWin7_32bit = "http://view.atdmt.com/action/UMIRF_IE_IE9HP_Win7?href=http://download.microsoft.com/download/8/6/D/86DB5DC9-5706-4A5B-BD46-FFBA6FA67D44/IE9-Windows7-x86-enu.exe"
+#$urlToWin7_64bit = "http://view.atdmt.com/action/UMIRF_IE_IE9HP_Win7?href=http://download.microsoft.com/download/8/6/D/86DB5DC9-5706-4A5B-BD46-FFBA6FA67D44/IE9-Windows7-x64-enu.exe"
+#downloads the whole thing and gives you the option of no.
+$urlToWin7_32bit = "http://view.atdmt.com/action/UMIRF_IE_IE9WW_InternationalDL?href=http://download.microsoft.com/download/C/3/B/C3BF2EF4-E764-430C-BDCE-479F2142FC81/IE9-Windows7-x86-enu.exe"
+$urlToWin7_64bit = "http://view.atdmt.com/action/UMIRF_IE_IE9WW_InternationalDL?href=http://download.microsoft.com/download/C/1/6/C167B427-722E-4665-9A40-A37BC5222B0A/IE9-Windows7-x64-enu.exe"
 
 $tempDir = Join-Path $env:TEMP "IE9"
 if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+$file = Join-Path $tempDir "IE9.exe"
 
-$file = Join-Path $tempDir "IE9-Windows7-x64-enu.exe"
+$url = $urlToWin7_32bit;
+if ($is64bit) { $url = $urlToWin7_64bit;}
 
-Write-Host "Downloading IE9 for Windows7 x64 to $file from $url"
+Write-Host "Downloading IE9 for $winVersionName $bitType to $file from $url"
 
 #http://bartdesmet.net/blogs/bart/archive/2006/11/25/PowerShell-_2D00_-How-to-download-a-file_3F00_.aspx
 $downloader = new-object System.Net.WebClient
