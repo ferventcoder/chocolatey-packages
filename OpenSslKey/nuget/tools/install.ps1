63ed09bae008819a85f12482903d5ea6e522c27a
Rob Reynolds <ferventcoder@gmail.com>
2011-03-29 09:47:45 -0500

    updating the notes during install

--- OpenSslKey/nuget/tools/install.ps1
+++ OpenSslKey/nuget/tools/install.ps1
@@ -1,5 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
 Write-Host 'Open SSL Key - Utility to generate XML keys from PEM or DER keys.'
-Write-Host 'Also comes with an exe application. Please install using chocolatey (you can get with install-package chocolatey)'
-Write-Host 'i.e. chocolatey install opensslkey'
+Write-Host 'Also comes with an exe application - Please install using chocolatey'
+Write-Host "To get chocolatey just run 'Install-Package chocolatey' followed by 'Initialize-Chocolatey'"
+Write-Host 'chocolatey install opensslkey'
--- launchy/tools/install.ps1
+++ launchy/tools/install.ps1
@@ -1,7 +1,8 @@
 param($installPath, $toolsPath, $package, $project)
 
 Write-Host 'This is not a package with dlls to reference in it.'
-Write-Host 'Please install using chocolatey (Install-Package chocolatey)'
+Write-Host 'Please install using chocolatey' 
+Write-Host "To get chocolatey just run 'Install-Package chocolatey' followed by 'Initialize-Chocolatey'"
 Write-Host 'chocolatey install launchy'
 write-host 'Removing this package...'
 uninstall-package launchy -ProjectName $project.Name
