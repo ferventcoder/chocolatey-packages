5620225ea35421ea5265b02ce46c34a9421363e3
Rob Reynolds <ferventcoder@gmail.com>
2011-03-30 23:38:25 -0500

    adding a warmup template for chocolatey

---
+++ _template/chocolatey/tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+﻿$fileName = '__NAME__'
+$fileType = '__REPLACE__' #msi or exe
+
+$chocTempDir = Join-Path $env:TEMP "chocolatey"
+$tempDir = Join-Path $chocTempDir "$fileName"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+$file = Join-Path $tempDir "$fileName.$fileType"
+
+$url = '__REPLACE__'
+
+Write-Host "Downloading $fileName to $file from $url"
+
+$downloader = new-object System.Net.WebClient
+$downloader.DownloadFile($url, $file)
+
+write-host "Installing $fileName silently..."
+if ($fileType -like 'msi') {
+  msiexec /i  "$file" /quiet
+}
+if ($fileType -like 'exe') {
+& "$file" "/S" #"/s /S /q /Q /quiet /silent" # try any of these to get the silent installer
+}
+
+write-host "$fileName has been installed."
+Start-Sleep 3
