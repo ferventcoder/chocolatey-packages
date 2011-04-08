299c1d74999a8fc3fe8d5eed763d42c138c3a8ea
Rob Reynolds <ferventcoder@gmail.com>
2011-04-08 15:44:41 -0500

    adding ilmerge

---
+++ ilmerge/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+$fileName = 'ilmerge'
+$fileType = 'msi' #msi or exe
+
+$chocTempDir = Join-Path $env:TEMP "chocolatey"
+$tempDir = Join-Path $chocTempDir "$fileName"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+$file = Join-Path $tempDir "$fileName.$fileType"
+
+$url = 'http://www.microsoft.com/downloads/info.aspx?na=41&SrcFamilyId=22914587-B4AD-4EAE-87CF-B14AE6A939B0&SrcDisplayLang=en&u=http%3a%2f%2fdownload.microsoft.com%2fdownload%2f1%2f3%2f4%2f1347C99E-9DFB-4252-8F6D-A3129A069F79%2fILMerge.msi'
+
+Write-Host "Downloading $fileName to $file from $url"
+
+$downloader = new-object System.Net.WebClient
+$downloader.DownloadFile($url, $file)
+
+write-host "Installing $fileName silently..."
+msiexec /i  "$file" /quiet
+
+write-host "$fileName has been installed."
+Start-Sleep 3
