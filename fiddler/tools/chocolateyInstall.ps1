97281c97269f9f1b831043aa0620afc4a20bdea8
Rob Reynolds <ferventcoder@gmail.com>
2011-04-25 02:19:36 -0500

    adding fiddler

---
+++ fiddler/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+try {
+  Get-ChildItem 'C:\NuGet\chocolateyInstall\helpers' -Filter *.psm1 | ForEach-Object { import-module -name  $_.FullName }
+  Install-ChocolateyPackage 'fiddler' 'exe' '/S' 'http://www.getfiddler.com/dl/Fiddler2Setup.exe'
+
+} catch {
+@"
+Error Occurred: $($_.Exception.Message)
+"@ | Write-Host -ForegroundColor White -BackgroundColor DarkRed
+	Start-Sleep 5
+	throw 
+}
