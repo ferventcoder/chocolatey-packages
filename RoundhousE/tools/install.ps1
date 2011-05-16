f9c396e8355071188b3effd5b2a8c25cd4b799db
Rob Reynolds <ferventcoder@gmail.com>
2011-05-16 17:20:33 -0500

    removing roundhouse as it was migrated back to its own source

--- RoundhousE/tools/install.ps1
+++
@@ -1,8 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-Write-Host 'This is not a package with dlls to reference in it.'
-Write-Host 'RoundhousE is a tool that can be local with your project or installed to the machine. If you want to do a machine install, please install using chocolatey' 
-Write-Host "To get chocolatey just run 'Install-Package chocolatey' followed by 'Initialize-Chocolatey'"
-Write-Host 'chocolatey install roundhouse'
-Write-Host 'If you are wanting to add this to your project for deployment/migrations, you may keep it where it is and pull it in with your build scripts or manually move it somewhere more manageable.'
-Write-Host 'In the future this script will do more towards automatically moving adding itself to a more conventional location when installed with vanilla #nuget.'
