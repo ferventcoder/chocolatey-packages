62394bd735824d3e23e92b9a78f7cfb73f2a4a86
Rob Reynolds <ferventcoder@gmail.com>
2011-05-25 14:47:29 -0500

    adding foobar2000 after being recommended by someone. Pretty sweet.

---
+++ foobar2000/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+Install-ChocolateyPackage 'foobar2000' 'exe' '/S' 'http://www.foobar2000.org/files/aaf36ecc0eedec46dc46f4e741bcfe0e/foobar2000_v1.1.6.exe' 
+#http://www.msfn.org/board/topic/39769-foobar2000-silent/
+ 
