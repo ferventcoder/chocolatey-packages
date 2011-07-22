2a3104ad9ef9078eab5cbeb8007e336716170e2b
Rob Reynolds <ferventcoder@gmail.com>
2011-07-21 19:55:11 -0500

    adding node js

---
+++ nodejs/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+try { 
+  $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $nodePath = Join-Path $scriptPath 'node.exe'
+  Get-ChocolateyWebFile 'nodejs' "$nodePath" 'http://nodejs.org/dist/v0.5.1/node.exe'
+ Write-ChocolateySuccess 'nodejs'
+} catch {
+  Write-ChocolateyFailure 'nodejs' "$($_.Exception.Message)"
+  throw 
+}
