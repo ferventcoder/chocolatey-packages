e525b2616d0604911604d5a6eb3a403b803d9de3
Rob Reynolds <ferventcoder@gmail.com>
2011-07-22 08:30:43 -0500

    updating nodejs to v0.5.2

--- nodejs/tools/chocolateyInstall.ps1
+++ nodejs/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 try { 
   $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
   $nodePath = Join-Path $scriptPath 'node.exe'
-  Get-ChocolateyWebFile 'nodejs' "$nodePath" 'http://nodejs.org/dist/v0.5.1/node.exe'
+  Get-ChocolateyWebFile 'nodejs' "$nodePath" 'http://nodejs.org/dist/v0.5.2/node.exe'
  Write-ChocolateySuccess 'nodejs'
 } catch {
   Write-ChocolateyFailure 'nodejs' "$($_.Exception.Message)"
