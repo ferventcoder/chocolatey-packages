65c17b1aa3c6e250b7cf1661cd1f030ec757329d
Rob Reynolds <ferventcoder@gmail.com>
2011-08-12 12:29:14 -0500

    updating ruby to 1.9.2.290

--- ruby/tools/chocolateyInstall.ps1
+++ ruby/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'ruby' 'exe' "/silent /tasks=`"assocfiles,modpath`"" 'http://rubyforge.org/frs/download.php/74298/rubyinstaller-1.9.2-p180.exe'
+Install-ChocolateyPackage 'ruby' 'exe' "/silent /tasks=`"assocfiles,modpath`"" 'http://rubyforge.org/frs/download.php/75127/rubyinstaller-1.9.2-p290.exe'
