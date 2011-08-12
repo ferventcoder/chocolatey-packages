c8ea58cbe4ad30528a251602fd0ebfb3e754f943
Rob Reynolds <ferventcoder@gmail.com>
2011-08-11 22:17:50 -0500

    updating nodejs to the latest version

--- nodejs/tools/chocolateyInstall.ps1
+++ nodejs/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 try { 
   $nodePath = Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'node.exe'
-  Get-ChocolateyWebFile 'nodejs' "$nodePath" 'http://nodejs.org/dist/v0.5.2/node.exe'
+  Get-ChocolateyWebFile 'nodejs' "$nodePath" 'http://nodejs.org/dist/v0.5.3/node.exe'
   
   Write-ChocolateySuccess 'nodejs'
 } catch {
