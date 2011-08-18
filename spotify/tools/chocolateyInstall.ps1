22dfa06c2235e4d681a343d79c9135610f6a7518
Rob Reynolds <ferventcoder@gmail.com>
2011-08-18 14:08:39 -0500

    attempting to remove the failure file to see if it successfully finished

--- spotify/tools/chocolateyInstall.ps1
+++ spotify/tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
 try {
   Install-ChocolateyPackage 'spotify' 'exe' '/S' 'http://download.spotify.com/Spotify%20Installer.exe'
 } catch {
+  Write-Host 'Spotify installer is reporting error, even though it may have installed correctly. Checking for existence...'
   #------this likes to claim issues even though it installs correctly ----------------
   $processor = Get-WmiObject Win32_Processor
   $is64bit = $processor.AddressWidth -eq 64
@@ -12,6 +13,8 @@ try {
 
   If (test-path $spotifyExe) {
     Write-ChocolateySuccess 'spotify'
+    $errorLog = "$($env:TEMP)\chocolatey\spotify\failure.log"
+    if ([System.IO.File]::Exists($errorLog)) {[System.IO.File]::Move($errorLog,(Join-Path ($errorLog) '.old'))}
   } else {
     Write-ChocolateyFailure 'spotify' "$($_.Exception.Message)"
     throw 
