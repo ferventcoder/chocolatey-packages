0fc3f3976d032fdac839d41fd4a8b1196d2ebae7
Rob Reynolds <ferventcoder@gmail.com>
2011-11-12 09:24:31 -0600

    updating postgresql installer to add path installation

--- postgresql/tools/chocolateyInstall.ps1
+++ postgresql/tools/chocolateyInstall.ps1
@@ -4,16 +4,21 @@ try {
   
   
   
-  #create postgres user with password Postgres1234
+  #create postgres user
   #Could do this - http://blogs.technet.com/b/heyscriptingguy/archive/2010/11/23/use-powershell-to-create-local-user-accounts.aspx | http://www.yusufozturk.info/windows-server/how-to-create-windows-user-with-powershell-2.html | http://stackoverflow.com/questions/383390/create-local-user-with-powershell-windows-vista
   $postgreAccount = 'postgres'
+  $postgrePassword = 'Postgres1234'
   Write-Host "Deleting and recreating $postgreAccount windows account..."
   try {
     net user $postgreAccount /delete
   } catch {
     Write-Host "User $postgreAccount doesn`'t exist"
   }
-  net user $postgreAccount Postgres1234 /add
+  net user $postgreAccount $postgrePassword /add
+  
+  Write-Host "The account $postgreAccount has been created with the password set to $postgrePassword. Please change the password for the $postgreAccount account and update the services to that password"
+  Start-Sleep 4
+  
   #create sysdrive\PostgreSQL folder
   $postgrePath = "$($env:SystemDrive)\PostgreSQL"
   Write-Host "Creating $postgrePath folder for installation if it doesn`'t exist"
@@ -26,11 +31,12 @@ try {
   $acl.AddAccessRule($rule);
   Set-Acl $postgrePath $acl
   
-  Install-ChocolateyPackage 'postgresql' 'exe' "--mode unattended --prefix $postgrePath --superpassword Postgres1234" 'http://get.enterprisedb.com/postgresql/postgresql-9.1.1-1-windows.exe' 
+  #perform silent install
+  Install-ChocolateyPackage 'postgresql' 'exe' "--mode unattended --prefix $postgrePath --superpassword $postgrePassword" 'http://get.enterprisedb.com/postgresql/postgresql-9.1.1-1-windows.exe' 
 #'http://get.enterprisedb.com/postgresql/postgresql-9.1.1-1-windows-x64.exe' 
 
-  Write-Host "Please change the password for the PostgreSQL account and update the services to that password"
-
+  #Add path
+  Install-ChocolateyPath "$($postgrePath)\bin"
   
   Write-ChocolateySuccess 'postgresql'
 } catch {
