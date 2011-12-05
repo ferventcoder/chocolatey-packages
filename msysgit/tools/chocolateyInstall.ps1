5f4d41cfbad776cf581325aea20b80e7ef9bb6b7
Rob Reynolds <ferventcoder@gmail.com>
2011-12-04 22:39:49 -0600

    updating msysgit to 1.7.7.1

--- msysgit/tools/chocolateyInstall.ps1
+++ msysgit/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 try {
-  Install-ChocolateyPackage 'msysgit' 'exe' '/SILENT' 'http://msysgit.googlecode.com/files/Git-1.7.7-preview20111012.exe' 
+  Install-ChocolateyPackage 'msysgit' 'exe' '/SILENT' 'http://msysgit.googlecode.com/files/Git-1.7.7.1-preview20111027.exe' 
 
   #------- ADDITIONAL SETUP -------#
   $processor = Get-WmiObject Win32_Processor
