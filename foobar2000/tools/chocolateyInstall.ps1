70edab7ecace0ceb72466e4edd42732189514323
Rob Reynolds <ferventcoder@gmail.com>
2011-12-04 22:19:09 -0600

    updating foobar2000 to 1.1.10

--- foobar2000/tools/chocolateyInstall.ps1
+++ foobar2000/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
-Install-ChocolateyPackage 'foobar2000' 'exe' '/S' 'http://www.foobar2000.org/files/d419bc4b1a5b3677570e14d78bfa047c/foobar2000_v1.1.7.exe' 
+Install-ChocolateyPackage 'foobar2000' 'exe' '/S' 'http://www.foobar2000.org/files/a606a7b89c4a2392c012b121f05949b4/foobar2000_v1.1.10.exe' 
 #http://www.msfn.org/board/topic/39769-foobar2000-silent/
  
