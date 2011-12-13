6b321f24fed926e87be6d4bc2c65a0f4233d5dec
Rob Reynolds <ferventcoder@gmail.com>
2011-12-13 12:03:26 -0600

    fixed spotify pkg

--- spotify/tools/chocolateyInstall.ps1
+++ spotify/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ try {
   $is64bit = $processor.AddressWidth -eq 64
 
   $progFiles = [System.Environment]::GetFolderPath('ProgramFiles')
-  if ($is64bit) {$progFiles = "$progFiles (x86)"}
+  if ($is64bit -and $progFiles -notmatch 'x86') {$progFiles = "$progFiles (x86)"}
 
   $spotifyExe = Join-Path $progFiles "Spotify\spotify.exe"
 
