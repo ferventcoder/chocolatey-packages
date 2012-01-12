32201b5d4bb07968a5a7a1359efa9863675c839c
Rob Reynolds <ferventcoder@gmail.com>
2012-01-12 08:17:13 -0600

    updating PHP

--- php/tools/chocolateyInstall.ps1
+++ php/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 write-host 'Please make sure you have CGI installed in IIS'
-Install-ChocolateyPackage 'php' 'msi' "/passive INSTALLDIR=$($env:SystemDrive)\php ADDLOCAL=cgi,ext_php_mysqli,iis4CGI,iis4FastCGI,ext_php_mysql,ext_php_xsl,ext_php_ldap,ext_php_curl,ext_php_xmlrpc,ext_php_sqlite,ext_php_sqlite3,ext_php_openssl" 'http://windows.php.net/downloads/releases/php-5.3.6-nts-Win32-VC9-x86.msi'
+Install-ChocolateyPackage 'php' 'msi' "/passive INSTALLDIR=$($env:SystemDrive)\php ADDLOCAL=cgi,ext_php_mysqli,iis4CGI,iis4FastCGI,ext_php_mysql,ext_php_xsl,ext_php_ldap,ext_php_curl,ext_php_xmlrpc,ext_php_sqlite,ext_php_sqlite3,ext_php_openssl" 'http://windows.php.net/downloads/releases/php-5.3.9-nts-Win32-VC9-x86.msi'
 
 
