772eb9e692eace483a409f87010080c240e70e3e
Alan Stevens <alanstevens@gmail.com>
2012-01-16 17:12:30 -0500

    Updated path variable to use appropriate 32/64bit environment variable

--- 7zip/tools/chocolateyInstall.ps1
+++ 7zip/tools/chocolateyInstall.ps1
@@ -1,3 +1,11 @@
 ﻿Install-ChocolateyPackage '7Zip' 'msi' '/quiet' 'http://sourceforge.net/projects/sevenzip/files/7-Zip/9.22/7z922.msi' 'http://sourceforge.net/projects/sevenzip/files/7-Zip/9.22/7z922-x64.msi'
 
-Install-ChocolateyPath  "$(join-path $env:systemdrive 'program files\7-Zip')"
+$programFiles = $env:ProgramFiles
+
+$is64bit = (Get-WmiObject Win32_OperatingSystem).OSArchitecture -eq '64-bit'
+
+if ($is64bit) {
+	$programFiles = $env:ProgramW6432;
+   }
+
+Install-ChocolateyPath  "$(join-path $programFiles '7-Zip')"
