683dfc25833650995236abb5e3023768514f3a73
Rob Reynolds <ferventcoder@gmail.com>
2012-01-16 18:47:02 -0600

    removing unnecessary files

--- 7zip/tools/chocolateyInstall.ps1
+++ 7zip/tools/chocolateyInstall.ps1
@@ -1,11 +1,17 @@
-﻿Install-ChocolateyPackage '7Zip' 'msi' '/quiet' 'http://sourceforge.net/projects/sevenzip/files/7-Zip/9.22/7z922.msi' 'http://sourceforge.net/projects/sevenzip/files/7-Zip/9.22/7z922-x64.msi'
+﻿try {
+  Install-ChocolateyPackage '7Zip' 'msi' '/quiet' 'http://sourceforge.net/projects/sevenzip/files/7-Zip/9.22/7z922.msi' 'http://sourceforge.net/projects/sevenzip/files/7-Zip/9.22/7z922-x64.msi'
 
-$programFiles = $env:ProgramFiles
+  $programFiles = $env:ProgramFiles
 
-$is64bit = (Get-WmiObject Win32_OperatingSystem).OSArchitecture -eq '64-bit'
+  $is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
+  if ($is64bit) {
+    $programFiles = $env:ProgramW6432;
+  }
 
-if ($is64bit) {
-	$programFiles = $env:ProgramW6432;
-   }
+  Install-ChocolateyPath  "$(join-path $programFiles '7-Zip')"
 
-Install-ChocolateyPath  "$(join-path $programFiles '7-Zip')"
+  Write-ChocolateySuccess '7Zip'
+} catch {
+  Write-ChocolateyFailure '7Zip' $($_.Exception.Message)
+  throw 
+}
