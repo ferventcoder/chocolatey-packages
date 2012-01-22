b8e778366d91e9a1b5d9a3e47c55cb0ffd11341e
Rob Reynolds <ferventcoder@gmail.com>
2012-01-22 05:48:55 -0600

    updating notepad++ to 5.9.8

--- notepadplusplus/tools/chocolateyInstall.ps1
+++ notepadplusplus/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage 'notepadplusplus' 'exe' '/S' 'http://download.tuxfamily.org/notepadplus/5.9.6.2/npp.5.9.6.2.Installer.exe'
+﻿Install-ChocolateyPackage 'notepadplusplus' 'exe' '/S' 'http://download.tuxfamily.org/notepadplus/5.9.8/npp.5.9.8.Installer.exe'
