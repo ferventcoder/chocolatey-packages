93eb0b4886e667406997a1e12b12ed522fd85c29
Alan Stevens <alanstevens@gmail.com>
2012-01-23 00:57:11 -0500

    Updated vim install to use a single inf file for all platforms

--- vim/content/gvim.cmd
+++ vim/content/gvim.cmd
@@ -1,14 +1,11 @@
 @echo off
 setlocal ENABLEEXTENSIONS
-IF DEFINED PROGRAMFILES(X86) SET VIMDIR=%PROGRAMFILES(X86)%\vim\vim73
-IF NOT DEFINED PROGRAMFILES(X86) SET VIMDIR=%PROGRAMFILES%\vim\vim73
-for %%A in ("%VIMDIR%") do set VIMDIR=%%~sA
 IF "%1" NEQ "" (GOTO:OPENFILEINTAB) ELSE (GOTO:OPENEMPTYTAB)
 
 :OPENFILEINTAB
-START %VIMDIR%\gvim.exe --remote-tab-silent "%*"
+START VIM_DIRECTORY\gvim.exe --remote-tab-silent "%*"
 GOTO:EOF
 
 :OPENEMPTYTAB
-FOR /F "tokens=*" %%i in ('%VIMDIR%\vim.exe --serverlist') do SET SERVERLIST=%%i
-IF "%SERVERLIST%" NEQ "" (%VIMDIR%\gvim.exe --remote-send "<C-\><C-N>:tabnew<CR>") ELSE (start %VIMDIR%\gvim.exe)
+FOR /F "tokens=*" %%i in ('VIM_DIRECTORY\vim.exe --serverlist') do SET SERVERLIST=%%i
+IF "%SERVERLIST%" EQU "" (start VIM_DIRECTORY\gvim.exe) ELSE (VIM_DIRECTORY\gvim.exe --remote-send "<C-\><C-N>:tabnew<CR>")
