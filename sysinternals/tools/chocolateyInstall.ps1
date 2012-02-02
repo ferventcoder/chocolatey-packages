a81d48153c798cb326a9cb05af27aac905cd4c39
Alan Stevens <alanstevens@gmail.com>
2012-02-02 15:10:11 -0500

    Fixed errors when sysinternals had not been previously installed.

--- sysinternals/tools/chocolateyInstall.ps1
+++ sysinternals/tools/chocolateyInstall.ps1
@@ -15,6 +15,9 @@ $nugetBin = join-path $env:chocolateyinstall 'bin'
 $files = get-childitem $installDir -include *.exe -recurse
 foreach ($file in $files) {
   try {
-    remove-item $(join-path $nugetBin ($file.Name.Replace(".exe",".bat").Replace(".EXE",".bat")))
+      $batchFile = join-path $nugetBin ($file.Name.Replace(".exe",".bat").Replace(".EXE",".bat"))
+      if(test-path $batchFile){
+        remove-item $batchFile
+    }
   }catch {}
 }
