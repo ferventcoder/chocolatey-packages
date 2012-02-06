c8af82640631f4d10cc60115896a581eb5bb1031
Rob Reynolds <ferventcoder@gmail.com>
2012-02-06 14:16:46 -0600

    updating foobar2000 to the latest version

--- foobar2000/tools/chocolateyInstall.ps1
+++ foobar2000/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
-Install-ChocolateyPackage 'foobar2000' 'exe' '/S' 'http://www.foobar2000.org/files/a606a7b89c4a2392c012b121f05949b4/foobar2000_v1.1.10.exe' 
+Install-ChocolateyPackage 'foobar2000' 'exe' '/S' 'http://www.foobar2000.org/files/a49e3ebfb1f1c7ca62d6816870be7c1b/foobar2000_v1.1.11.exe' 
 #http://www.msfn.org/board/topic/39769-foobar2000-silent/
  
--- foobar2000/tools/install.ps1
+++
@@ -1,8 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-Write-Host 'This is not a package with dlls to reference in it.'
-Write-Host 'Please install using chocolatey' 
-Write-Host "To get chocolatey just run 'Install-Package chocolatey' followed by 'Initialize-Chocolatey'"
-Write-Host 'chocolatey install foobar2000'
-write-host 'Removing this package...'
-uninstall-package foobar2000 -ProjectName $project.Name
