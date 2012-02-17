6452834516a1d65e3335e82c20114b0aa2986371
Rob Reynolds <ferventcoder@gmail.com>
2012-02-17 01:48:29 -0600

    updating ruby to 1.8.7.358

--- ruby/tools/chocolateyInstall.ps1
+++ ruby/tools/chocolateyInstall.ps1
@@ -5,26 +5,33 @@ try {
   if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
 
   $rubyFolder = '187'
-  $url = 'http://rubyforge.org/frs/download.php/75679/rubyinstaller-1.8.7-p357.exe'
+  $url = 'http://rubyforge.org/frs/download.php/75851/rubyinstaller-1.8.7-p358.exe'
 
   #$rubyFolder = '192'
   #$url = 'http://rubyforge.org/frs/download.php/75127/rubyinstaller-1.9.2-p290.exe'
 
+  #$rubyFolder = '193'
+  #$url = 'http://rubyforge.org/frs/download.php/75848/rubyinstaller-1.9.3-p125.exe'
+
   $rubyPath = join-path $binRoot $('ruby' + "$rubyFolder")
-  $silentArgs = "/silent /dir=`"$rubyPath`""
+  $silentArgs = "/silent /dir=`"$rubyPath`" /tasks=`"assocfiles,modpath`""
 
   Install-ChocolateyPackage 'ruby' 'exe' "$silentArgs" "$url"
 
-  # Install and configure pik
-  #$rubyBin = join-path $rubyPath 'bin'
-  #$nugetBin = join-path $env:chocolateyinstall 'bin'
-  #$gem = join-path $rubyBin 'gem.bat'
-  #$pikInstall = join-path $rubyBin 'pik_install.bat'
-  #& $gem install pik
-  #& $pikInstall "$nugetBin"
+  # # Install and configure pik
+  # Write-Host "Now we are going to install pik and set up the folder - so Ruby is pointed to the correct version"
+  # $rubyBin = join-path $rubyPath 'bin'
+  # Write-Host "Temporarily adding `'$rubyBin`' to the path so pik doesn't fail after we install it"
+  # $env:Path = "$($env:Path);$rubyBin"
+  
+  # $nugetBin = join-path $env:ChocolateyInstall 'bin'
+  # #$gem = 'gem.bat'
+  # $pikInstall = 'pik_install.bat'
+  # & gem install pik
+  # & $pikInstall "$nugetBin"
 
-  #& pik add $rubyBin
-  #& pik use $rubyFolder
+  # & pik add $rubyBin
+  # & pik use $rubyFolder
 
   Write-ChocolateySuccess 'ruby'
 } catch {
