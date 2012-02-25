98a87aa0b1391fd2923f5cb36f8eb1c75cf98aff
Rob Reynolds <ferventcoder@gmail.com>
2012-02-25 12:30:57 -0600

    adding a nodejs.commandline - pointing nodejs to it

--- nodejs/tools/chocolateyInstall.ps1
+++ nodejs/tools/chocolateyInstall.ps1
@@ -1,9 +1,1 @@
-try { 
-  $nodePath = Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'node.exe'
-  Get-ChocolateyWebFile 'nodejs' "$nodePath" 'http://nodejs.org/dist/v0.6.11/node.exe'
-  
-  Write-ChocolateySuccess 'nodejs'
-} catch {
-  Write-ChocolateyFailure 'nodejs' "$($_.Exception.Message)"
-  throw 
-}
+#Install-VirtualPackage 'nodejs.commandline' 'nodejs.install'
