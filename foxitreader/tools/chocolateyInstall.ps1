2ed217f8fae224eb0ae7682816875a66f6e1ac4f
Rob Reynolds <ferventcoder@gmail.com>
2012-02-25 10:22:28 -0600

    fox it reader - I don't own this one...

--- foxitreader/tools/chocolateyInstall.ps1
+++ foxitreader/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'foxitreader' 'exe' '/silent' 'http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/win/5.x/5.0/enu/FoxitReader502.0718_enu_Setup.exe'
+Install-ChocolateyPackage 'foxitreader' 'exe' '/silent' 'http://cdn04.foxitsoftware.com/pub/foxit/reader/desktop/win/5.x/5.1/enu/FoxitReader514.0104_enu_Setup.exe'
