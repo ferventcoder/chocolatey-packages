4a5b27f689f70a54f4f69dbf868d5f112418f3bd
Rob Reynolds <ferventcoder@gmail.com>
2012-02-25 09:34:07 -0600

    updating filezilla to 3.5.3

--- filezilla/tools/chocolateyInstall.ps1
+++ filezilla/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'filezilla' 'exe' '/S' 'http://downloads.sourceforge.net/filezilla/FileZilla_3.5.2_win32-setup.exe'
+Install-ChocolateyPackage 'filezilla' 'exe' '/S' 'http://downloads.sourceforge.net/filezilla/FileZilla_3.5.3_win32-setup.exe'
