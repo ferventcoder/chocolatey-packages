cdcd0e3b060e93a80021a6fa4e199f8fba00490c
Rob Reynolds <ferventcoder@gmail.com>
2012-02-25 07:02:47 -0600

    updating adobe reader

--- adobereader/tools/chocolateyInstall.ps1
+++ adobereader/tools/chocolateyInstall.ps1
@@ -3,4 +3,4 @@
 # '/sPB /msi /norestart ALLUSERS=1 EULA_ACCEPT=YES'
 #'/sAll /rs /msi "/qb-! /norestart ALLUSERS=1 EULA_ACCEPT=YES SUPPRESS_APP_LAUNCH=YES"' 
 #'/msi /norestart /quiet'
-Install-ChocolateyPackage 'adobereader' 'exe' '/sPB /msi /norestart /quiet ALLUSERS=1 EULA_ACCEPT=YES' 'http://ardownload.adobe.com/pub/adobe/reader/win/10.x/10.1.1/en_US/AdbeRdr1011_en_US.exe'
+Install-ChocolateyPackage 'adobereader' 'exe' '/sPB /msi /norestart /quiet ALLUSERS=1 EULA_ACCEPT=YES' 'http://ardownload.adobe.com/pub/adobe/reader/win/10.x/10.1.2/en_US/AdbeRdr1012_en_US.exe'
