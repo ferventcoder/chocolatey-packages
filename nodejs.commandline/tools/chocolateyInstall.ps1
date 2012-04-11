c9620335ee02959baea00c8c93fc073d33f7002b
Rob Reynolds <ferventcoder@gmail.com>
2012-04-11 05:44:50 -0500

    nodejs 0.6.15

--- nodejs.commandline/tools/chocolateyInstall.ps1
+++ nodejs.commandline/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 try { 
   $nodePath = Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'node.exe'
-  Get-ChocolateyWebFile 'nodejs' "$nodePath" 'http://nodejs.org/dist/v0.6.14/node.exe'
+  Get-ChocolateyWebFile 'nodejs' "$nodePath" 'http://nodejs.org/dist/v0.6.15/node.exe'
   
   Write-ChocolateySuccess 'nodejs'
 } catch {
