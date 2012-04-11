fc998060ebfbb026f1c825a84d27f013584b191e
Rob Reynolds <ferventcoder@gmail.com>
2012-04-11 05:32:35 -0500

    updating virtualbox to 4.1.12

--- virtualbox/tools/chocolateyInstall.ps1
+++ virtualbox/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage "virtualbox" "exe" "-s" "http://download.virtualbox.org/virtualbox/4.1.8/VirtualBox-4.1.8-75467-Win.exe"
+Install-ChocolateyPackage "virtualbox" "exe" "-s" "http://download.virtualbox.org/virtualbox/4.1.12/VirtualBox-4.1.12-77245-Win.exe"
