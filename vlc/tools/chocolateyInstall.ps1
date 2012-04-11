496891d8685e14ec73959c151218fa69c08bc17a
Rob Reynolds <ferventcoder@gmail.com>
2012-04-11 06:16:58 -0500

    vlc 2.0.1

--- vlc/tools/chocolateyInstall.ps1
+++ vlc/tools/chocolateyInstall.ps1
@@ -1,1 +1,2 @@
-﻿Install-ChocolateyPackage 'vlc' 'exe' '/S' 'http://downloads.sourceforge.net/project/vlc/1.1.11/win32/vlc-1.1.11-win32.exe?r=http%3A%2F%2Fwww.videolan.org%2Fvlc%2F&ts=1323059614&use_mirror=superb-sea2'
+﻿Install-ChocolateyPackage 'vlc' 'exe' '/S' 'http://downloads.sourceforge.net/project/vlc/2.0.1/win32/vlc-2.0.1-win32.exe'
+#http://downloads.sourceforge.net/project/vlc/2.0.1/win32/vlc-2.0.1-win32.exe?r=http%3A%2F%2Fwww.videolan.org%2Fvlc%2F&ts=1334143044&use_mirror=hivelocity
