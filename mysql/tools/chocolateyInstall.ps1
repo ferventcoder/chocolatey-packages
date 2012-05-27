e836d2ef67b579ddf62308b60ed7de558925e005
Rob Reynolds <ferventcoder@gmail.com>
2012-05-27 02:22:51 -0500

    moved to choco auto pkg

--- mysql/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-try { 
-  $toolsDir ="$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  Start-ChocolateyProcessAsAdmin "& $($toolsDir)\installmysql.ps1"
-
-  Write-ChocolateySuccess 'mysql'
-} catch {
-  Write-ChocolateyFailure 'mysql' "$($_.Exception.Message)"
-  throw 
-}
