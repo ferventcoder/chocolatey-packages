f9206e01a52887f4fd5d887d24ae8a032d24afd9
Rob Reynolds <ferventcoder@gmail.com>
2012-05-31 00:21:21 -0500

    moved skype to an auto package

--- skype/tools/chocolateyInstall.ps1
+++
@@ -1,2 +0,0 @@
-#Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://download.skype.com/3694814915aaa38100bfa0933f948e65/partner/59/SkypeSetup.exe'
-Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://download.skype.com/3694814915aaa38100bfa0933f948e65/partner/59/SkypeSetupFull.exe'
