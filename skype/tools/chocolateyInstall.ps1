deb92d84519235996dfa0e78bdf79d9feef5ab4e
Rob Reynolds <ferventcoder@gmail.com>
2012-05-31 00:20:47 -0500

    skype auto pkg

---
+++ skype/tools/chocolateyInstall.ps1
@@ -0,0 +1,2 @@
+#Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://download.skype.com/3694814915aaa38100bfa0933f948e65/partner/59/SkypeSetup.exe'
+Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' '{{DownloadUrl}}'
