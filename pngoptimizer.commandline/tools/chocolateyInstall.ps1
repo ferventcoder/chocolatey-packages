66dfcbebae800434f07753fcf976ad81456ed5ce
Rob Reynolds <ferventcoder@gmail.com>
2012-06-28 09:50:38 -0500

    pngoptimizer 2.1 packages

---
+++ pngoptimizer.commandline/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyZipPackage 'pngoptimizer' 'http://psydk.org/download/PngOptimizerCL-2.1-x64.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 'http://psydk.org/download/PngOptimizerCL-2.1-x64.zip'
