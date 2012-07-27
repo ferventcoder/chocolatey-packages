b1241e7c285e0aebaf2687708dcec0897a8ca292
Rob Reynolds <ferventcoder@gmail.com>
2012-07-27 12:16:36 -0500

    instant eyedropper 1.75

---
+++ instanteyedropper.app/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage 'instanteyedropper' 'exe' '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 'http://instant-eyedropper.com/download/InstantEyedropper.exe'  -validExitCodes @(0)
