be56818fea72880801da46b8a2d4060066368c72
Rob Reynolds <ferventcoder@gmail.com>
2012-09-04 09:29:24 -0500

    fixing the download url for foobar2000

--- foobar2000/tools/chocolateyInstall.ps1
+++ foobar2000/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
-Install-ChocolateyPackage 'foobar2000' 'exe' '/S' 'http://www.foobar2000.org/files/a49e3ebfb1f1c7ca62d6816870be7c1b/foobar2000_v1.1.11.exe' 
+Install-ChocolateyPackage 'foobar2000' 'exe' '/S' '{{DownloadUrl}}' 
 #http://www.msfn.org/board/topic/39769-foobar2000-silent/
  
