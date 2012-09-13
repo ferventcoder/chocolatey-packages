6225cec1385186151276680f2d3a96e62e31861b
Rob Reynolds <ferventcoder@gmail.com>
2012-09-13 16:20:02 -0500

    updating nodejs.install to put itself on the path

--- nodejs.install/tools/chocolateyInstall.ps1
+++ nodejs.install/tools/chocolateyInstall.ps1
@@ -1,1 +1,6 @@
 Install-ChocolateyPackage 'nodejs.install' 'msi' '/quiet' '{{DownloadUrl}}'
+
+$nodePath = "$env:SystemDrive\Program Files\nodejs"
+if (![System.IO.Directory]::Exists($nodePath)) {$nodePath = "$env:SystemDrive\Program Files (x86)\nodejs";}
+
+$env:Path = "$($env:Path);$nodePath"
