372d81f4a8de0dc0fa152c5b32907e64520eb834
Rob Reynolds <ferventcoder@gmail.com>
2012-09-13 15:22:38 -0500

    use bin_dir

--- optipng/tools/chocolateyInstall.ps1
+++ optipng/tools/chocolateyInstall.ps1
@@ -1,10 +1,17 @@
 try {
   $package = 'OptiPNG'
-  $unzipPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  $url = 'http://prdownloads.sourceforge.net/optipng/optipng-0.7.1-win32.zip?download'
-  Install-ChocolateyZipPackage $package $url $unzipPath
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+  ### For BinRoot, use the following instead ###
+  $binRoot = "$env:systemdrive\"
+  ### Using an environment variable to to define the bin root until we implement configuration ###
+  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $installDir = Join-Path $binRoot $package
+  Write-Host "Adding `'$installDir`' to the path and the current shell path"
+  Install-ChocolateyPath "$installDir"
+  $env:Path = "$($env:Path);$installDir"
+  $zipUrl = 'http://prdownloads.sourceforge.net/optipng/optipng-0.7.1-win32.zip?download'
 
-  Install-ChocolateyPath $unzipPath
+  Install-ChocolateyZipPackage $package "$zipUrl" "$installDir"
 
   Write-ChocolateySuccess $package
 } catch {
--- phantomjs/tools/chocolateyInstall.ps1
+++ phantomjs/tools/chocolateyInstall.ps1
@@ -1,11 +1,19 @@
 try {
   $package = 'PhantomJS'
-  $unzipPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  $url = 'http://phantomjs.googlecode.com/files/phantomjs-1.6.1-win32-static.zip'
-  Install-ChocolateyZipPackage $package $url $unzipPath
 
-  Install-ChocolateyPath $unzipPath
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+  ### For BinRoot, use the following instead ###
+  $binRoot = "$env:systemdrive\"
+  ### Using an environment variable to to define the bin root until we implement configuration ###
+  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $installDir = Join-Path $binRoot $package
+  Write-Host "Adding `'$installDir`' to the path and the current shell path"
+  Install-ChocolateyPath "$installDir"
+  $env:Path = "$($env:Path);$installDir"
+  $zipUrl = 'http://phantomjs.googlecode.com/files/phantomjs-1.6.1-win32-static.zip'
 
+  Install-ChocolateyZipPackage $package "$zipUrl" "$installDir"
+  
   Write-ChocolateySuccess $package
 } catch {
   Write-ChocolateyFailure $package "$($_.Exception.Message)"
