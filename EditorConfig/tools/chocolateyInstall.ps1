8dcddf54fab3b0eb651e19449955f1e6c880ef8f
Iristyle <Iristyle@github>
2012-09-18 18:38:25 -0400

    Added EditorConfig 0.2.5 VS Extension
    - EditorConfig supports the use of .editorconfig files for formatting
    - This attempts to install in VS 2010 and VS 2012 (though there) is
    an issue with their manifest that prevents the VS2012 install
    - VS2012 fails silently
    - Image has been OptiPNG'd

---
+++ EditorConfig/tools/chocolateyInstall.ps1
@@ -0,0 +1,39 @@
+#some code "borrowed" from
+#https://raw.github.com/alanstevens/ChocoPackages/master/alanstevens.vsextensions/tools/ChocolateyInstall.ps1
+$packageName = 'EditorConfig'
+
+#look for all VS2010 and 2012
+$installers = @('HKCU:\Software\Microsoft\VisualStudio\10.0_Config',
+  'HKCU:\Software\Microsoft\VisualStudio\11.0_Config') |
+  ? { Test-Path $_ } |
+  % {
+    $installerPath = (Get-ItemProperty "$($_)\Setup\VS").EnvironmentDirectory
+    Join-Path $installerPath 'vsixinstaller.exe'
+  } |
+  ? { Test-Path $_ }
+
+try
+{
+  $tempDir = "$env:TEMP\chocolatey\editorConfig"
+  if (!(Test-Path $tempDir)) { New-Item $tempDir -Type Directory }
+
+  $params = @{
+    packageName = $packageName;
+    fileFullPath = Join-Path $tempDir "EditorConfigPlugin-0.2.5.vsix";
+    url = 'http://visualstudiogallery.msdn.microsoft.com/c8bccfe2-650c-4b42-bc5c-845e21f96328/file/75539/6/EditorConfigPlugin-0.2.5.vsix';
+  }
+
+  Get-ChocolateyWebFile @params
+
+  $installers |
+    % {
+      Write-Host "Installing `'$($params.fileFullPath)`' with $_"
+      $vsParams = '/q', $params.fileFullPath
+      &$_ $vsParams | Out-Null
+    }
+
+  Write-ChocolateySuccess $packageName
+} catch {
+  Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+  throw
+}
