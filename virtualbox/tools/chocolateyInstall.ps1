52c64485bca09ba9d8e266aba0ba4bc07299540b
Rob Reynolds <ferventcoder@gmail.com>
2012-10-26 10:41:43 -0500

    starting to change virtualbox to extract and install so we can get a no reboot option

--- virtualbox/tools/chocolateyInstall.ps1
+++ virtualbox/tools/chocolateyInstall.ps1
@@ -1,1 +1,23 @@
-Install-ChocolateyPackage 'virtualbox' 'exe' '-s' '{{DownloadUrl}}'
+$packageName = 'virtualbox'
+
+Install-ChocolateyPackage $packageName 'exe' '-s' '{{DownloadUrl}}'
+
+try {
+ # create the temp dir
+ #$tempDir = #some folder under temp\chocolatey
+ # name file fullpath
+ #$fileFullPath = Join-Path $tempDir 'virtualbox.exe'
+ # get the file
+ # Get-ChocolateyWebFile $packageName $fileFullPath '{{DownloadUrl}}'
+ # extract the contents
+ #. $fileFullPath -x -p $tempDir
+ # find the proper msi
+ #$msiFile = $tempDir #find it
+ # run it
+ #Install-ChocolateyInstallPackage $packageName 'msi' '/quiet /norestart' $msiFile 
+
+  Write-ChocolateySuccess $packageName
+} catch {
+  Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+  throw 
+}
