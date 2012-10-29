4c314a396ee7672c167f7bc046dd0277f66d36d3
Rob Reynolds <ferventcoder@gmail.com>
2012-10-29 12:13:37 -0500

    adding notes to php:

--- php/tools/chocolateyInstall.ps1
+++ php/tools/chocolateyInstall.ps1
@@ -3,4 +3,4 @@
 write-host 'Please make sure you have CGI installed in IIS'
 Install-ChocolateyPackage 'php' 'msi' "/passive INSTALLDIR=$($env:SystemDrive)\php ADDLOCAL=cgi,ext_php_mysqli,iis4CGI,iis4FastCGI,ext_php_mysql,ext_php_xsl,ext_php_ldap,ext_php_curl,ext_php_xmlrpc,ext_php_sqlite,ext_php_sqlite3,ext_php_openssl" 'http://windows.php.net/downloads/releases/php-{{PackageVersion}}-nts-Win32-VC9-x86.msi'
 
-
+#,php_mbstring,php_mongo,php_soap,php_pdo_mysql
