31968645522a948579e52c38e9b73811f2e0491f
Iristyle <Iristyle@Github>
2012-11-02 14:28:11 -0400

    Mono 2.10.9

--- mono/tools/chocolateyInstall.ps1
+++ mono/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'mono' 'exe' '/SILENT' 'http://download.mono-project.com/archive/2.10.8/windows-installer/1/mono-2.10.8-gtksharp-2.12.11-win32-1.exe' 
+Install-ChocolateyPackage 'mono' 'exe' '/SILENT' 'http://download.mono-project.com/archive/2.10.9/windows-installer/0/mono-2.10.9-gtksharp-2.12.11-win32-0.exe'
