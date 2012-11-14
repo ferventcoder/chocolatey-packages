543f7a85e0dfd9c3d7b441da3c14421ad648ac20
Rob Reynolds <ferventcoder@gmail.com>
2012-11-14 08:25:41 -0600

    resharper 7.1.25.234

--- resharper/tools/chocolateyInstall.ps1
+++ resharper/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'resharper' 'msi' '/quiet' 'http://download.jetbrains.com/resharper/ReSharperSetup.7.0.1098.2760.msi' 
+Install-ChocolateyPackage 'resharper' 'msi' '/quiet' 'http://download.jetbrains.com/resharper/ReSharperSetup.7.1.25.234.msi' 
