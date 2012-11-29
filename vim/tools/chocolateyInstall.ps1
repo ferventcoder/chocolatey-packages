060efb0675796eed9283d60477352da1e7a24fa9
Justin Dearing <zippy1981@gmail.com>
2012-11-29 21:45:35 +0000

    Updated to version 7.3.736. Also batch files (vim.bat, gvim.bat, etc) are created in C:\chocolatey\bin.

--- vim/tools/chocolateyInstall.ps1
+++ vim/tools/chocolateyInstall.ps1
@@ -1,6 +1,16 @@
 $packageName = 'vim'
 $fileType = 'exe'
-$silentArgs = '/S'
-$url = 'http://downloads.sourceforge.net/project/cream/Vim/7.3.401/gvim-7-3-401.exe'
+$args = '/S'
+$url = 'http://downloads.sourceforge.net/project/cream/Vim/7.3.736/gvim-7-3-736.exe'
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+Install-ChocolateyPackage $packageName $fileType $args $url
+
+$installDir = Split-Path -Parent (Get-ItemProperty HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Vim UninstallString).UninstallString
+# special batch files we want to create
+$diffExeIgnore = Join-Path $installDir "diff.exe.ignore"
+$uninstallExeIgnore = Join-Path $installDir "uninstall.exe.ignore"
+$gvimExeGui = Join-Path $installDir "gvim.exe.gui"
+
+New-Item $diffExeIgnore,$uninstallExeIgnore,$gvimExeGui -Type File -Force | Out-Null
+Get-ChocolateyBins $installDir
+Remove-Item $diffExeIgnore,$uninstallExeIgnore,$gvimExeGui
