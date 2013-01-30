f96c49108ad5e802fbee98fd6f5248ddd223ab3c
Rob Reynolds <ferventcoder@gmail.com>
2013-01-30 08:28:17 -0600

    pulling in Matt Wrock's changes to virtual box

--- virtualbox/tools/chocolateyInstall.ps1
+++ virtualbox/tools/chocolateyInstall.ps1
@@ -1,23 +1,4 @@
 $packageName = 'virtualbox'
-
+$tools="$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$tools\oracle.cer'"
 Install-ChocolateyPackage $packageName 'exe' '-s' '{{DownloadUrl}}'
-
-try {
- # create the temp dir
- #$tempDir = #some folder under temp\chocolatey
- # name file fullpath
- #$fileFullPath = Join-Path $tempDir 'virtualbox.exe'
- # get the file
- # Get-ChocolateyWebFile $packageName $fileFullPath '{{DownloadUrl}}'
- # extract the contents
- #. $fileFullPath -x -p $tempDir
- # find the proper msi
- #$msiFile = $tempDir #find it
- # run it
- #Install-ChocolateyInstallPackage $packageName 'msi' '/quiet /norestart' $msiFile 
-
-  Write-ChocolateySuccess $packageName
-} catch {
-  Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
-  throw 
-}
