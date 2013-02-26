6a65acc7443e93edcc658d82fdccdbe9f515dbe9
Rob Reynolds <ferventcoder@gmail.com>
2013-02-26 13:35:00 -0600

    nodejs updates

--- _output/nodejs.install/0.8.21/tools/chocolateyInstall.ps1
+++ _output/nodejs.install/0.8.21/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'nodejs.install' 
 $installerType = 'msi'
-$url = 'http://nodejs.org/dist/v{{PackageVersion}}/node-v{{PackageVersion}}-x86.msi' 
-$url64 = 'http://nodejs.org/dist/v{{PackageVersion}}/x64/node-v{{PackageVersion}}-x64.msi' 
+$url = 'http://nodejs.org/dist/v0.8.21/node-v0.8.21-x86.msi' 
+$url64 = 'http://nodejs.org/dist/v0.8.21/x64/node-v0.8.21-x64.msi' 
 $silentArgs = '/quiet' 
 $validExitCodes = @(0) 
 
