ae22800b8baed0b3bbaef100928af5bad5598198
Alan Stevens <alanstevens@gmail.com>
2013-04-01 14:13:42 -0400

    Added icon and path to VirtualBox

--- virtualbox/tools/chocolateyInstall.ps1
+++ virtualbox/tools/chocolateyInstall.ps1
@@ -2,3 +2,11 @@ $packageName = 'virtualbox'
 $tools="$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$tools\oracle.cer'"
 Install-ChocolateyPackage $packageName 'exe' '-s' '{{DownloadUrl}}'
+
+$is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
+$programFiles = $env:programfiles
+if ($is64bit) {$programFiles = ${env:ProgramFiles(x86)}}
+$fsObject = New-Object -ComObject Scripting.FileSystemObject
+$programFiles = $fsObject.GetFolder("$programFiles").ShortPath
+
+Install-ChocolateyPath "$programfiles\Oracle\VirtualBox"
