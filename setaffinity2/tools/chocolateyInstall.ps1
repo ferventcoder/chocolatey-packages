4bb6be5dde199ccad6892e4a080a9d8a55bc55ca
Rob Reynolds <ferventcoder@gmail.com>
2013-04-22 08:54:02 -0500

    updating SetAffinity2

--- setaffinity2/tools/chocolateyInstall.ps1
+++ setaffinity2/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 try { 
-
   $toolsDir ="$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  Install-ChocolateyZipPackage 'setaffinity2' 'http://edgemeal.110mb.com/Files/Set_Affinity_II_1.041.zip' $toolsDir
+  #$zipFile = join-path $toolsDir 'Set_Affinity_II_1.041.zip'
+  Install-ChocolateyZipPackage 'setaffinity2' "http://www.bbses.info/mirrors/edgemeal.110mb.com/Files/Set_Affinity_II_1.041.zip" $toolsDir
   
   Rename-Item "$($toolsDir)\Set Affinity II.exe" 'setaffinity2.exe'
   
