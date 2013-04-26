cf0e7e633eb12d4c7690ff1c77dd3f4f6b7f8998
Rob Reynolds <ferventcoder@gmail.com>
2013-04-26 07:26:18 -0500

    auto package updates

---
+++ _output/foobar2000/1.2.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+Install-ChocolateyPackage 'foobar2000' 'exe' '/S' 'http://www.foobar2000.org/files/11c9ec0f58568bb916555180bae03eb1/foobar2000_v1.2.5.exe' 
+#http://www.msfn.org/board/topic/39769-foobar2000-silent/
+ 
