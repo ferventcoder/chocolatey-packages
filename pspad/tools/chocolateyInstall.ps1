b193fde8c08ef00967696c7f20adb4c4e33bbe7b
Rob Reynolds <ferventcoder@gmail.com>
2013-05-05 17:06:49 -0500

    pspad 4.5.7

---
+++ pspad/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿
+$packageName = 'pspad' 
+$installerType = 'exe'
+$url = 'http://pspad.siliconhill.cz/files/pspad457inst_en.exe' 
+$silentArgs = ' /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
