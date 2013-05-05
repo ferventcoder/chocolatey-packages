cb5391d75c8d4cb224d795db2b029bb2ae9c56e3
Rob Reynolds <ferventcoder@gmail.com>
2013-05-05 15:32:09 -0500

    updates

---
+++ _output/foobar2000/1.2.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+Install-ChocolateyPackage 'foobar2000' 'exe' '/S' 'http://www.foobar2000.org/files/ae398d8035b4527b706faa75c8ac42ca/foobar2000_v1.2.6.exe' 
+#http://www.msfn.org/board/topic/39769-foobar2000-silent/
+ 
