399078b24d016a44a144d43573c5f2314028d582
Rob Reynolds <ferventcoder@gmail.com>
2013-05-13 12:01:00 -0500

    updating skype to use full setup

--- _output/skype/6.3.0.105/tools/chocolateyInstall.ps1
+++ _output/skype/6.3.0.105/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 #Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://download.skype.com/3694814915aaa38100bfa0933f948e65/partner/59/SkypeSetup.exe'
-Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://fs40.filehippo.com/2008/d6ed22c488154862b981bc5801865474/SkypeSetup.exe'
+Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://download.skype.com/SkypeSetupFull.exe'
 
 #'http://www.filehippo.com/download/file/7f0f87c3e2f2258adfed235a1dc9ab6c83c2db5b6767ad094fb51c592cf2d71e'
--- skype/tools/chocolateyInstall.ps1
+++ skype/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 #Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://download.skype.com/3694814915aaa38100bfa0933f948e65/partner/59/SkypeSetup.exe'
 #Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' '{{DownloadUrl}}'
-Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://download.skype.com/SkypeSetup.exe'
+Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://download.skype.com/SkypeSetupFull.exe'
