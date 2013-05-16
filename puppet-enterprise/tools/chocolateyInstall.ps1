ca471817bbe307194a34f30f6972e80d7079c81e
Rob Reynolds <ferventcoder@gmail.com>
2013-05-16 09:20:47 -0500

    puppet enterprise

---
+++ puppet-enterprise/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'puppet-enterprise' 
+$installerType = 'msi'
+$url = 'http://pm.puppetlabs.com/puppet-enterprise/2.8.1/puppet-enterprise-2.8.1.msi' 
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
