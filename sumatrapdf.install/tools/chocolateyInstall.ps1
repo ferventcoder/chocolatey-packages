52f5b5721492b716c8f32e0f9b0e3040608edda9
TomOne <user.email@fake.com>
2013-05-28 02:10:53 +0200

    updated sumatrapdf to v2.3.2, changed charset of nuspec files to UTF-8

--- sumatrapdf.install/tools/chocolateyInstall.ps1
+++ sumatrapdf.install/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage 'sumatrapdf.install' 'exe' '/S' 'https://kjkpub.s3.amazonaws.com/sumatrapdf/rel/SumatraPDF-2.2-install.exe' -validExitCodes @(0)
+﻿Install-ChocolateyPackage 'sumatrapdf.install' 'exe' '/S' 'https://kjkpub.s3.amazonaws.com/sumatrapdf/rel/SumatraPDF-2.3.2-install.exe' -validExitCodes @(0)
