8b4012d8653fb28777deec9b4eedf320133ca816
TomOne <user.email@fake.com>
2013-05-28 23:13:13 +0200

    foobar2000 package now uses filehippo as download source, changed nuspec to UTF-8, better icon

--- foobar2000/tools/chocolateyInstall.ps1
+++ foobar2000/tools/chocolateyInstall.ps1
@@ -1,3 +1,23 @@
-Install-ChocolateyPackage 'foobar2000' 'exe' '/S' '{{DownloadUrl}}' 
-#http://www.msfn.org/board/topic/39769-foobar2000-silent/
- 
+﻿try {
+
+    $packageName = "foobar2000"
+    $fileType = "exe"
+    $silentArgs = "/S"
+    $pwd = "$(split-path -parent $MyInvocation.MyCommand.Definition)"
+    
+	Import-Module "$($pwd)\Get-FilenameFromRegex.ps1"
+	# Why does an import failure on this module not throw an error?
+
+    $url1 = Get-FilenameFromRegex "http://www.filehippo.com/download_foobar2000/" 'download_foobar2000/download/([\w\d]+)/' 'http://www.filehippo.com/de/download_foobar2000/download/$1/'
+    Write-Host "Found URL which contains the download URL: $url1"
+
+	$url2 = Get-FilenameFromRegex "$url1" '/download/file/([\w\d]+)/' 'http://www.filehippo.com/download/file/$1/'
+	Write-Host "Found download URL: $url2"
+
+	Install-ChocolateyPackage $packageName $fileType $silentArgs $url2
+
+    Write-ChocolateySuccess $name
+} catch {
+  Write-ChocolateyFailure $name $($_.Exception.Message)
+  throw
+}
