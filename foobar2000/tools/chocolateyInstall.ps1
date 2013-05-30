0a8025ab5d4266899b663b461a0f5baa6994e772
Rob Reynolds <ferventcoder@gmail.com>
2013-05-30 16:50:44 -0500

    setting the download link to look for english version

--- foobar2000/tools/chocolateyInstall.ps1
+++ foobar2000/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@
 	Import-Module "$($pwd)\Get-FilenameFromRegex.ps1"
 	# Why does an import failure on this module not throw an error?
 
-    $url1 = Get-FilenameFromRegex "http://www.filehippo.com/download_foobar2000/" 'download_foobar2000/download/([\w\d]+)/' 'http://www.filehippo.com/de/download_foobar2000/download/$1/'
+    $url1 = Get-FilenameFromRegex "http://www.filehippo.com/download_foobar2000/" 'download_foobar2000/download/([\w\d]+)/' 'http://www.filehippo.com/en/download_foobar2000/download/$1/'
     Write-Host "Found URL which contains the download URL: $url1"
 
 	$url2 = Get-FilenameFromRegex "$url1" '/download/file/([\w\d]+)/' 'http://www.filehippo.com/download/file/$1/'
