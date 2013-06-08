3fccdbf5d21f3fc89ad5c29758574025826edecf
Rob Reynolds <ferventcoder@gmail.com>
2013-06-08 08:23:25 -0500

    mysql now using the zip file since there is no silent installer.

--- mysql/tools/installmysql.ps1
+++ mysql/tools/installmysql.ps1
@@ -1,20 +1,15 @@
 $packageName = 'mysql'
 $packageType = 'msi'
 $silentArgs = '/passive'
-$downloadUrl = "http://cdn.mysql.com/Downloads/MySQLInstaller/mysql-installer-community-{{PackageVersion}}.msi"
-
-#$downloadUrl64 = ""
-
-#http://www.mysql.com/get/Downloads/MySQL-5.5/mysql-{{PackageVersion}}-win32.msi/from/http://mysql.he.net/' 
-#'http://www.mysql.com/get/Downloads/MySQL-5.5/mysql-{{PackageVersion}}-winx64.msi/from/http://mysql.he.net/'
+$downloadUrl = 'http://cdn.mysql.com/Downloads/MySQLInstaller/mysql-installer-community-{{PackageVersion}}.msi'
 
 try {
-  Install-ChocolateyPackage "$packageName" "$packageType" "$silentArgs" "$downloadUrl" #"$downloadUrl64"
+  Install-ChocolateyPackage "$packageName" "$packageType" "$silentArgs" "$downloadUrl"
   
   #------- ADDITIONAL SETUP -------#
   #add it to the path
-  $mysqlPath = "$env:SystemDrive\Program Files\MySQL\MySQL Server 5.5\bin"
-  if (![System.IO.Directory]::Exists($mysqlPath)) {$mysqlPath = "$env:SystemDrive\Program Files (x86)\MySQL\MySQL Server 5.5\bin";}
+  $mysqlPath = "$env:SystemDrive\Program Files\MySQL\MySQL Server 5.6\bin"
+  if (![System.IO.Directory]::Exists($mysqlPath)) {$mysqlPath = "$env:SystemDrive\Program Files (x86)\MySQL\MySQL Server 5.6\bin";}
   Install-ChocolateyPath $mysqlPath 'Machine'
 
   #install the service itself
@@ -22,8 +17,8 @@ try {
   #turn on the service
   Start-Process -FilePath "NET" -ArgumentList 'START MySQL' -Wait -NoNewWindow
   
-  Write-ChocolateySuccess "$packageName
+  Write-ChocolateySuccess "$packageName"
 } catch {
-  Write-ChocolateyFailure "$packageName $($_.Exception.Message)
+  Write-ChocolateyFailure "$packageName" $($_.Exception.Message)
   throw 
 }
