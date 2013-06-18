12afb1485c4ebe790867216e2cd74657a5cbdb1e
Rob Reynolds <ferventcoder@gmail.com>
2013-06-18 13:32:47 -0500

    fixing foobar 1.2.7 and 1.2.8

--- _output/foobar2000/1.2.8/tools/chocolateyInstall.ps1
+++ _output/foobar2000/1.2.8/tools/chocolateyInstall.ps1
@@ -8,13 +8,17 @@
 	Import-Module "$($pwd)\Get-FilenameFromRegex.ps1"
 	# Why does an import failure on this module not throw an error?
 
-    $url1 = Get-FilenameFromRegex "http://www.filehippo.com/download_foobar2000/" 'download_foobar2000/download/([\w\d]+)/' 'http://www.filehippo.com/en/download_foobar2000/download/$1/'
-    Write-Host "Found URL which contains the download URL: $url1"
+	$url1 = Get-FilenameFromRegex "http://www.filehippo.com/download_foobar2000/history/" 'download_foobar2000/([\d]+)/">Foobar2000 1.2.8<' 'http://www.filehippo.com/download_foobar2000/$1/'
+	
+    Write-Host "Found URL which contains the download URL 1: $url1"
 
-	$url2 = Get-FilenameFromRegex "$url1" '/download/file/([\w\d]+)/' 'http://www.filehippo.com/download/file/$1/'
-	Write-Host "Found download URL: $url2"
+    $url2 = Get-FilenameFromRegex "$url1" 'download_foobar2000/download/([\w\d]+)/' 'http://www.filehippo.com/en/download_foobar2000/download/$1/'
+    Write-Host "Found URL which contains the download URL 2: $url2"
 
-	Install-ChocolateyPackage $packageName $fileType $silentArgs $url2
+	$url3 = Get-FilenameFromRegex "$url2" '/download/file/([\w\d]+)/' 'http://www.filehippo.com/download/file/$1/'
+	Write-Host "Found download URL: $url3"
+
+	Install-ChocolateyPackage $packageName $fileType $silentArgs $url3
 
     Write-ChocolateySuccess $name
 } catch {
