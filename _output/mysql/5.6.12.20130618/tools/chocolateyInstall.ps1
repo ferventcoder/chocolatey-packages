e5afc6ef46470213a2e7bdc8c0386fcea771e0fc
Rob Reynolds <ferventcoder@gmail.com>
2013-06-18 12:52:15 -0500

    ensuring we pass the 64 bit url with mysql package

--- _output/mysql/5.6.12.20130618/tools/chocolateyInstall.ps1
+++ _output/mysql/5.6.12.20130618/tools/chocolateyInstall.ps1
@@ -12,8 +12,8 @@
 $packageName = 'mysql'
 $packageType = 'msi'
 $silentArgs = '/passive'
-$url = 'http://cdn.mysql.com/Downloads/MySQL-5.6/mysql-{{PackageVersion}}-win32.zip'
-$url64 = 'http://cdn.mysql.com/Downloads/MySQL-5.6/mysql-{{PackageVersion}}-winx64.zip'
+$url = 'http://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.12-win32.zip'
+$url64 = 'http://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.12-winx64.zip'
 
 try {
 
