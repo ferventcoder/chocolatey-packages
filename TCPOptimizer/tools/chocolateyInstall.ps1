af689fd73cc23cef02f8e7913c4b77360aa29e71
Rob Reynolds <ferventcoder@gmail.com>
2013-06-20 16:37:07 -0500

    tcpoptimizer

---
+++ TCPOptimizer/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'TCPOptimizer'
+$url = 'http://www.speedguide.net/files/TCPOptimizer.zip'
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
