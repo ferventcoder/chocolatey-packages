35ec31bc81a1019316e01786f23849997971658d
TomOne <user.email@fake.com>
2013-07-12 19:57:17 +0200

    add filezilla.commandline

---
+++ filezilla.commandline/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'filezilla.commandline'
+$url = 'http://sourceforge.net/projects/filezilla/files/FileZilla_Client/3.7.1/FileZilla_3.7.1_win32.zip/download'
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
