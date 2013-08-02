dadc85db3ebab5711193d4e1e046f40e5df3e568
TomOne <user.email@fake.com>
2013-08-02 13:16:59 +0200

    added optipng package

---
+++ OptiPNG/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+try {
+  $package = 'OptiPNG'
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+  ### For BinRoot, use the following instead ###
+  $binRoot = "$env:systemdrive\"
+  ### Using an environment variable to to define the bin root until we implement configuration ###
+  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $installDir = Join-Path $binRoot $package
+  Write-Host "Adding `'$installDir`' to the path and the current shell path"
+  Install-ChocolateyPath "$installDir"
+  $env:Path = "$($env:Path);$installDir"
+  $zipUrl = 'http://prdownloads.sourceforge.net/optipng/optipng-0.7.1-win32.zip?download'
+
+  Install-ChocolateyZipPackage $package "$zipUrl" "$installDir"
+
+  Copy-Item "$($installDir)\optipng-0.7.1-win32\*" "$installDir" -Force -Recurse
+  
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
