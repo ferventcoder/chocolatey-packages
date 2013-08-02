1beb1b28d8752125b1d9bf2a8abbebad27c8dbe5
TomOne <user.email@fake.com>
2013-08-02 20:00:18 +0200

    puppet to automatic package

--- puppet/tools/chocolateyinstall.ps1
+++ puppet/tools/chocolateyinstall.ps1
@@ -1,3 +1,2 @@
-#Install-ChocolateyPackage 'puppet' 'MSI' '/qn' 'http://puppetlabs.com/downloads/windows/puppet-2.7.21.msi' -validExitCodes @(0)
-Install-ChocolateyPackage 'puppet' 'MSI' '/qn' 'http://puppetlabs.com/downloads/windows/puppet-3.2.1.msi' -validExitCodes @(0)
+Install-ChocolateyPackage 'puppet' 'MSI' '/qn' '{{DownloadUrl}}' -validExitCodes @(0)
 
