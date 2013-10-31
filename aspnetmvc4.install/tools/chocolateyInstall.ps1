2423848c2575ec75130103a5aa1cb357a96a9170
Justin Dearing <zippy1981@gmail.com>
2013-10-31 13:48:38 -0400

    Added aspnetmvc4.install

---
+++ aspnetmvc4.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+Write-Host 'This will take forever to install. Go outside or something...come back in about an hour or so. For serious yo...'
+
+Install-ChocolateyPackage 'aspnetmvc4.install' 'exe' '/q' 'http://download.microsoft.com/download/2/F/6/2F63CCD8-9288-4CC8-B58C-81D109F8F5A3/AspNetMVC4Setup.exe'
+#/q but it takes forever....
