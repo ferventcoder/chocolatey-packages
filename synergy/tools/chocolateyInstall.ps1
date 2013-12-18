bfcdfb6acdd856940c7d14acceece952d5f63dc2
Rob Reynolds <ferventcoder@gmail.com>
2013-12-18 12:37:57 -0600

    synergy 1.4.14 beta

--- synergy/tools/chocolateyInstall.ps1
+++ synergy/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'synergy' 'exe' '/S' 'http://synergy.googlecode.com/files/synergy-1.4.12-Windows-x86.exe' 'http://synergy.googlecode.com/files/synergy-1.4.12-Windows-x64.exe' 
+Install-ChocolateyPackage 'synergy' 'exe' '/S' 'http://synergy.googlecode.com/files/synergy-1.4.14-Windows-x86.exe' 'http://synergy.googlecode.com/files/synergy-1.4.14-Windows-x64.exe' 
