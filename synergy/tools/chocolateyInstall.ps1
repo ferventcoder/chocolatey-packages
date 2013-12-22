b851d79758fc16fc30225b61fd2d6cbb0c729f46
Rob Reynolds <ferventcoder@gmail.com>
2013-12-21 21:45:58 -0600

    synergy 1.4.15

--- synergy/tools/chocolateyInstall.ps1
+++ synergy/tools/chocolateyInstall.ps1
@@ -1,1 +1,9 @@
-Install-ChocolateyPackage 'synergy' 'exe' '/S' 'http://synergy.googlecode.com/files/synergy-1.4.14-Windows-x86.exe' 'http://synergy.googlecode.com/files/synergy-1.4.14-Windows-x64.exe' 
+$packageName = 'synergy'
+$packageVersion = '1.4.15'
+$installerType = 'exe'
+$url   = "http://synergy.googlecode.com/files/synergy-$packageVersion-Windows-x86.exe"
+$url64 = "http://synergy.googlecode.com/files/synergy-$packageVersion-Windows-x64.exe"
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
