68e610c6d6d81619b275de628926d42ab13cddec
Rob Reynolds <ferventcoder@gmail.com>
2014-01-02 16:50:43 -0600

    iscommandlineapp v1.0

---
+++ iscommandlineapp/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'iscommandlineapp'
+$url = 'http://files.helgeklein.com/downloads/IsCommandLineApp/current/IsCommandLineApp.zip'
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
