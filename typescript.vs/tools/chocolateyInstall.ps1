2a785899524a4b1a0816e959314023cb3ff6e45d
Rob Reynolds <ferventcoder@gmail.com>
2014-01-12 16:28:41 -0600

    typescript 0.9.5

--- typescript.vs/tools/chocolateyInstall.ps1
+++ typescript.vs/tools/chocolateyInstall.ps1
@@ -1,1 +1,6 @@
-﻿Install-ChocolateyPackage 'typescript.vs' 'exe' '/quiet' 'http://download.microsoft.com/download/2/F/F/2FFA1FBA-97CA-4FFB-8ED7-A4AE06398948/TypeScriptSetup.0.9.1.1.exe' -validExitCodes @(0)
+﻿$packageName = "typescript.vs"
+$installerType = "exe"
+$silentArgs= "/quiet"
+$url = "http://download.microsoft.com/download/2/F/F/2FFA1FBA-97CA-4FFB-8ED7-A4AE06398948/TypeScriptSetup.0.9.5.exe"
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes @(0)
