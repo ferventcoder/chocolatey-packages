1cf64195afb964d85da3d95d8a45bb142817b92a
Rob Reynolds <ferventcoder@gmail.com>
2014-01-15 05:40:07 -0600

    updates prior to automatic run for 20140115_054002

---
+++ _output/adobereader/11.0.06/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+$silentArgs = '/sPB /msi /norestart /quiet ALLUSERS=1 EULA_ACCEPT=YES'
+$downloadUrl = 'http://ardownload.adobe.com/pub/adobe/reader/win/11.x/11.0.06/en_US/AdbeRdr11006_en_US.exe'
+#http://forums.adobe.com/thread/754256
+#http://www.appdeploy.com/messageboards/tm.asp?m=37416
+# '/sPB /msi /norestart ALLUSERS=1 EULA_ACCEPT=YES'
+#'/sAll /rs /msi "/qb-! /norestart ALLUSERS=1 EULA_ACCEPT=YES SUPPRESS_APP_LAUNCH=YES"' 
+#'/msi /norestart /quiet'
+Install-ChocolateyPackage 'adobereader' 'exe' "$silentArgs" "$downloadUrl"
