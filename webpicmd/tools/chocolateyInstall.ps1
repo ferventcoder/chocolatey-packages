ec1329b7f42f0006cd6b24535d691a8026e8dba7
Rob Reynolds <ferventcoder@gmail.com>
2014-01-18 15:21:17 -0600

    webpicmd updates

--- webpicmd/tools/chocolateyInstall.ps1
+++ webpicmd/tools/chocolateyInstall.ps1
@@ -1,8 +1,25 @@
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$packageName = 'webpicmd'
+$installerType = 'msi'
+$url   = 'http://download.microsoft.com/download/7/0/4/704CEB4C-9F42-4962-A2B0-5C84B0682C7A/WebPlatformInstaller_x86_en-US.msi'
+$url64 = 'http://download.microsoft.com/download/7/0/4/704CEB4C-9F42-4962-A2B0-5C84B0682C7A/WebPlatformInstaller_amd64_en-US.msi'
+$silentArgs = "/passive /norestart"
+
+$installDir = Join-Path "$toolsDir" 'webpi'
+$fileName = "$($packageName).msi"
+$file = Join-Path "$toolsDir" "$fileName"
+
 try {
- #download either 86 or 64 bit
- #x86: http://download.microsoft.com/download/7/0/4/704CEB4C-9F42-4962-A2B0-5C84B0682C7A/WebPlatformInstaller_x86_en-US.msi
- #x64: http://download.microsoft.com/download/7/0/4/704CEB4C-9F42-4962-A2B0-5C84B0682C7A/WebPlatformInstaller_amd64_en-US.msi
- #unpack with lessmsi /x
- #unpack to webpi
- #let choco do the rest
+  Get-ChocolateyWebFile "$packageName" "$file" "$url" "$url64"
+
+  pushd "$toolsDir"
+  # the trailing backslash is required!
+  &lessmsi x "$fileName" "$installDir\"
+  popd
+
+  Write-ChocolateySuccess $packageName
+}
+catch
+{
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
 }
--- webpicommandline/tools/chocolateyInstall.ps1
+++
@@ -1,3 +0,0 @@
-Install-ChocolateyPackage 'WebPICommandline' 'msi' '/passive /norestart' `
-    'http://download.microsoft.com/download/7/0/4/704CEB4C-9F42-4962-A2B0-5C84B0682C7A/WebPlatformInstaller_x86_en-US.msi'     `
-    'http://download.microsoft.com/download/7/0/4/704CEB4C-9F42-4962-A2B0-5C84B0682C7A/WebPlatformInstaller_amd64_en-US.msi'
