96bfe5a15484c148ab2aaffd70c46e1bc473a304
Matt Wrock <matt@mattwrock.com>
2014-01-17 23:42:35 -0800

    updates webpicommandline to latest version which does not require .net 3.5 unlike the current one on choco

--- webpicommandline/tools/chocolateyInstall.ps1
+++ webpicommandline/tools/chocolateyInstall.ps1
@@ -1,1 +1,3 @@
-Install-ChocolateyZipPackage 'webpicommandline' 'http://go.microsoft.com/fwlink/?LinkId=233753' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyPackage 'WebPICommandline' 'msi' '/passive /norestart' `
+    'http://download.microsoft.com/download/7/0/4/704CEB4C-9F42-4962-A2B0-5C84B0682C7A/WebPlatformInstaller_x86_en-US.msi'     `
+    'http://download.microsoft.com/download/7/0/4/704CEB4C-9F42-4962-A2B0-5C84B0682C7A/WebPlatformInstaller_amd64_en-US.msi'
