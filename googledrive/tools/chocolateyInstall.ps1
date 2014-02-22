47a23e4f5ef6bdf6bcc18a0d0c41fe6f75faba15
Rob Reynolds <ferventcoder@gmail.com>
2014-02-22 07:52:35 -0600

    googledrive 1.14.6059.0644

--- googledrive/tools/chocolateyInstall.ps1
+++ googledrive/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage 'googledrive' 'msi' '/quiet' '{{DownloadUrl}}' -validExitCodes @(0,3010)
+﻿Install-ChocolateyPackage 'googledrive' 'msi' '/quiet' 'https://dl.google.com/drive/{{PackageVersion}}/gsync.msi' -validExitCodes @(0,3010)
