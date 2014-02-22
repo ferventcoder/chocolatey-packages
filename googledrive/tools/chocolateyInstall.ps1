c29562e445a876d150c5803d2dfc02326a5437f8
Rob Reynolds <ferventcoder@gmail.com>
2014-02-22 08:27:34 -0600

    adding note about alternative location for gdrive

--- googledrive/tools/chocolateyInstall.ps1
+++ googledrive/tools/chocolateyInstall.ps1
@@ -1,1 +1,3 @@
 ﻿Install-ChocolateyPackage 'googledrive' 'msi' '/quiet' 'https://dl.google.com/drive/{{PackageVersion}}/gsync.msi' -validExitCodes @(0,3010)
+
+# One user suggested https://dl.google.com/drive/gsync_enterprise.msi
