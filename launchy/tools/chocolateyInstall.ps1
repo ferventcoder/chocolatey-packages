4e2d2c36d05b3bcf518b4730ded1305615146954
Rob Reynolds <ferventcoder@gmail.com>
2014-03-01 08:17:30 -0600

    launchy - VERYSILENT and checksum

--- launchy/tools/chocolateyInstall.ps1
+++ launchy/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage 'launchy' 'exe' '/silent' 'http://www.launchy.net/downloads/win/Launchy2.5.exe'
+﻿Install-ChocolateyPackage 'launchy' 'exe' '/VERYSILENT /NORESTART' 'http://www.launchy.net/downloads/win/Launchy2.5.exe' -checksum 'C67962F064924F3C7B95D69F88E745C0'
