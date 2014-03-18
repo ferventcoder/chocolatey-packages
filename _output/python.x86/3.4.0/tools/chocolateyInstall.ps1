8ce36f38c246fd007b5d0462fbc6800e4754a6b8
Rob Reynolds <ferventcoder@gmail.com>
2014-03-18 05:40:06 -0500

    updates prior to automatic run for 20140318_054000

---
+++ _output/python.x86/3.4.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+try {
+  $binRoot = "$env:systemdrive\"
+
+  ### Using an environment variable to to define the bin root until we implement YAML configuration ###
+  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  $packageName = 'Python.x86'
+  $fileType = 'msi'
+  $silentArgs = "/qn TARGETDIR=$(join-path $binRoot 'Python27')"
+  $url = 'http://www.python.org/ftp/python/3.4.0/python-3.4.0.msi'
+
+  Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+
+  Install-ChocolateyPath $(join-path $binRoot 'Python27') 'User'
+
+  Write-ChocolateySuccess 'python.x86'
+} catch {
+  Write-ChocolateyFailure 'python.x86' "$($_.Exception.Message)"
+  throw
+}
