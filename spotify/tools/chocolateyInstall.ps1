511143227ff7d96c153cbf6023e7ceb17b0bde10
Matt Wrock <matt@mattwrock.com>
2014-04-22 00:20:49 -0700

    changing compatibility mode for spotify installer

--- spotify/tools/chocolateyInstall.ps1
+++ spotify/tools/chocolateyInstall.ps1
@@ -1,34 +1,21 @@
-﻿function Reset-ChocoEnvironment 
-{
-  #rename-item function:Start-ChocolateyProcessAsAdmin Start-ChocolateyProcessAsAdmin-SpotifyOverride
-  #rename-item function:Start-ChocolateyProcessAsAdmin-Hold Start-ChocolateyProcessAsAdmin
-  if (Get-Module spotify-chocolatey) {Remove-Module spotify-chocolatey}
-}
-
-try{
+﻿try{
   $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-  $spotifyModule = Join-Path $scriptDir 'functions\spotify-chocolatey.psm1'
-  if (Get-Module spotify-chocolatey) {Remove-Module spotify-chocolatey}
-  Import-Module $spotifymodule
-  
-  #Write-Host "Renaming `'Start-ChocolateyProcessAsAdmin`' to `'Start-ChocolateyProcessAsAdmin-Hold`'"
-  #rename-item function:Start-ChocolateyProcessAsAdmin Start-ChocolateyProcessAsAdmin-Hold
-  #rename-item function:Start-ChocolateyProcessAsAdmin-Override Start-ChocolateyProcessAsAdmin
-  
-  Install-ChocolateyPackage-Spotify 'spotify' 'exe' '/SILENT' 'http://download.spotify.com/SpotifySetup.exe'
+
+  #Changing compat mode to avoid dialog stating that installer must run as a non admin user
+  $env:__COMPAT_LAYER="WINXPSP3"
+  Install-ChocolateyPackage 'spotify' 'exe' '/SILENT' 'http://download.spotify.com/SpotifySetup.exe'
+  $env:__COMPAT_LAYER=$null
 
   $installerFile = Join-Path $scriptDir 'install.au3'
   write-host "Finishing spotify install with AutoIt3 using `'$installerFile`'"
   $installArgs = "/c autoit3 `"$installerFile`""
-  #Start-ChocolateyProcessAsAdmin "$installArgs" 'cmd.exe'
   Start-Process "cmd" -ArgumentList "$installArgs" -Wait
   
-  Reset-ChocoEnvironment
-
   Write-ChocolateySuccess 'spotify'
 } catch {
   Write-ChocolateyFailure 'spotify' "$($_.Exception.Message)"
-  Reset-ChocoEnvironment
-  
   throw 
+}
+finally {
+  $env:__COMPAT_LAYER=$null
 }
