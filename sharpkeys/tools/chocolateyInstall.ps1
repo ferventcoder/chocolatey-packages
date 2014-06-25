f09d0bc9ef0f50c26cb820c818582a6c024e17d0
Rob Reynolds <ferventcoder@gmail.com>
2014-06-25 10:41:31 -0500

    sharpkeys 3.5.0

--- sharpkeys/tools/chocolateyInstall.ps1
+++ sharpkeys/tools/chocolateyInstall.ps1
@@ -1,1 +1,6 @@
-Install-ChocolateyPackage 'sharpkeys' 'msi' '/S' 'http://www.randyrants.com/sharpkeys3.msi'
+$packageName = 'sharpkeys'
+$packageType = 'msi'
+$silentArgs = '/qn'
+$url = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=sharpkeys&DownloadId=319724&FileTime=129737577110830000&Build=20911'
+
+Install-ChocolateyPackage "$packageName" "$packageType" "$silentArgs" "$url"
