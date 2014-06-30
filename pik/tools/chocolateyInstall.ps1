3a483f48a2a8e321a4a0246c82403e05e78cbd25
Rob Reynolds <ferventcoder@gmail.com>
2014-06-30 07:34:22 -0500

    pik 0.3.0

--- pik/tools/chocolateyInstall.ps1
+++ pik/tools/chocolateyInstall.ps1
@@ -14,8 +14,10 @@
 
   $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
   $logFile = Join-Path $scriptDir 'pikinstall.log'
+  $silentArgs = "/quiet /norestart INSTALLDIR=$pikPath /l*v $logFile"
+  $url = 'https://github.com/downloads/vertiginous/pik/pik-0.3.0.pre.msi'
 
-  Install-ChocolateyPackage 'pik' 'msi' "/quiet /norestart INSTALLDIR=$pikPath /l*v $logFile" 'https://github.com/downloads/vertiginous/pik/pik-0.2.8.msi' #  -validExitCodes @(0)
+  Install-ChocolateyPackage 'pik' 'msi' "$silentArgs" "$url" -validExitCodes @(0,3010)
 
   Write-Host "Now you just need to call `'pik add Ruby_Install_Bin_Dir`' followed by `'pik use Ruby_Version_Number`'"
   Write-Host "For example, if you had ruby installed at C:\Ruby192, you would call `'pik add C:\Ruby192\bin`' followed by `'pik use 192`'"
