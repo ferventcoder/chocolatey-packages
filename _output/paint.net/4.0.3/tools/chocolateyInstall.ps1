4a9f6a8e2a282a0e5930f8f2f085b50a6719e87f
Rob Reynolds <ferventcoder@gmail.com>
2014-07-23 05:40:04 -0500

    updates prior to automatic run for 20140723_054000

---
+++ _output/paint.net/4.0.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+try {
+  $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  Install-ChocolateyZipPackage 'paint.net' 'http://www.dotpdn.com/files/paint.net.4.0.3.install.zip' $toolsDir
+
+  $paintFileFullPath = get-childitem $toolsDir -recurse -include *.exe | select -First 1
+  Install-ChocolateyInstallPackage 'paint.net' 'exe' '/auto DESKTOPSHORTCUT=0' "$paintFileFullPath"
+
+  Remove-Item "$paintFileFullPath"
+
+  Write-ChocolateySuccess 'paint.net'
+} catch {
+  Write-ChocolateyFailure 'paint.net' "$($_.Exception.Message)"
+  throw
+}
