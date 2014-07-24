a14cbec679f48a715be9a16e7357669120e84f28
Rob Reynolds <ferventcoder@gmail.com>
2014-07-24 11:42:17 -0500

    win2003-mklink downloads from official distribution

--- win2003-mklink/tools/chocolateyInstall.ps1
+++ win2003-mklink/tools/chocolateyInstall.ps1
@@ -1,9 +1,15 @@
-﻿try{
+﻿$packageName = 'win2003-mklink'
+$url = 'http://schinagl.priv.at/nt/hardlinkshellext/driver/symlink-1.06-x86.cab'
+$url64 = 'http://schinagl.priv.at/nt/hardlinkshellext/driver/symlink-1.06-x64.zip'
+
+try{
+  $thisDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $unzipLocation = Join-Path $thisDir "symlinks"
+  Install-ChocolateyZipPackage $packageName $url $unzipLocation -url64bit $url64
+  
   $chocoPath = $env:ChocolateyInstall
   $chocoPath = Join-Path $chocoPath 'bin'
-  $packageBatchFileName = Join-Path $chocoPath "mklink.bat"
-
-  $thisDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $packageBatchFileName = Join-Path $chocoPath "mklink.bat"  
   $path = Join-Path $thisDir 'symlinks\mklink.cmd'
   Write-Host "Adding $packageBatchFileName and pointing to $path"
 "@echo off
@@ -13,10 +19,10 @@
 ""`$SYSTEMROOT/System32/cmd.exe"" /c ""`$(basename `$0).bat `$*""
 exit `$?" | Out-File $packageBatchFileName.Replace(".bat","") -encoding ASCII
 
-  write-host "win2003-mklink is now ready. You can type 'mklink' from any command line at any path."
+  write-host "$packageName is now ready. You can type 'mklink' from any command line at any path."
 
-  Write-ChocolateySuccess 'win2003-mklink'
+  Write-ChocolateySuccess "$packageName"
 } catch {
-  Write-ChocolateyFailure 'win2003-mklink' "$($_.Exception.Message)"
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
   throw
 }
