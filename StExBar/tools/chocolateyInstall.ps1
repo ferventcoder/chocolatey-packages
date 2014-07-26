9d1aa596f0e1b9f649f7ff2e1cf063cf61e3d93f
Rob Reynolds <ferventcoder@gmail.com>
2014-07-26 10:42:11 -0500

    StExBar updates (fixes)

--- StExBar/tools/chocolateyInstall.ps1
+++ StExBar/tools/chocolateyInstall.ps1
@@ -1,1 +1,7 @@
-Install-ChocolateyPackage 'StExBar' 'msi' '/quiet' '{{DownloadUrl}}' '{{DownloadUrlx64}}' 
+$packageName = 'StExBar'
+$installerType = 'msi'
+$silentArgs = '/quiet /norestart'
+$url = 'http://sourceforge.net/projects/stefanstools/files/StExBar/StExBar-{{PackageVersion}}.msi/download'
+$url64 = 'http://sourceforge.net/projects/stefanstools/files/StExBar/StExBar64-{{PackageVersion}}.msi/download'
+
+Install-ChocolateyPackage $packageName $installerType "$silentArgs" "$url" "$url64"
