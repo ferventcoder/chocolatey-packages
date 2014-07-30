c30e7eb6cf69a5b76ac0d563aa83798adeba9106
Frode Nilsen <frode.nilsen@vg.no>
2014-07-30 09:43:58 +0200

    PHP uninstall: Spelling

--- automatic/php/tools/chocolateyUninstall.ps1
+++ automatic/php/tools/chocolateyUninstall.ps1
@@ -2,5 +2,5 @@ write-host 'Please make sure you have CGI installed in IIS'
 $packageName = '{{PackageName}}' 
 $validExitCodes = @(0)
 $targetFolder = Join-Path $(Get-BinRoot) $packageName
-Echo ("Deleting" + $targetFolder)
+Echo ("Deleting " + $targetFolder)
 Remove-Item -Force -Recurse $targetFolder
