6f2e9611c79e8fa529b026d2609ef4d93e9806be
Rob Reynolds <ferventcoder@gmail.com>
2014-08-18 12:18:46 -0500

    synergy 1.5.0 fixes for msi switch

--- manual/synergy/tools/chocolateyInstall.ps1
+++ manual/synergy/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 $packageName = 'synergy'
 $packageVersion = '1.5.0'
-$installerType = 'exe'
+$installerType = 'msi'
 $url   = "http://synergy-project.org/files/packages/synergy-1.5.0-r2278-Windows-x86.msi"
 $url64 = "http://synergy-project.org/files/packages/synergy-1.5.0-r2278-Windows-x64.msi"
-$silentArgs = '/S'
+$silentArgs = '/quiet /norestart'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
