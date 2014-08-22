0413d6ffb489c8852b00ed37c8a59c179553b5b9
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-08-22 10:44:28 -0400

    Remove unused variables in the uninstall script

--- manual/ruby.devkit/tools/chocolateyUninstall.ps1
+++ manual/ruby.devkit/tools/chocolateyUninstall.ps1
@@ -1,6 +1,3 @@
-$id = "ruby.devkit"
-$url = "https://github.com/downloads/oneclick/rubyinstaller/DevKit-tdm-32-4.5.2-20111229-1559-sfx.exe"
-
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $devkit = Join-Path (Get-BinRoot) "DevKit"
 
