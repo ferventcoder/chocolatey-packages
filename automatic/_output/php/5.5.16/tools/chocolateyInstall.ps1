4e6d866baec29b1d167675a62528ce40324b4bb5
Rob Reynolds <ferventcoder@gmail.com>
2014-08-22 05:40:05 -0500

    updates prior to automatic run for 20140822_054000

---
+++ automatic/_output/php/5.5.16/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+write-host 'Please make sure you have CGI installed in IIS'
+$packageName = 'php' 
+$url = 'http://windows.php.net/downloads/releases/php-5.5.16-nts-Win32-VC11-x86.zip' 
+$url64 = 'http://windows.php.net/downloads/releases/php-5.5.16-nts-Win32-VC11-x64.zip' 
+$validExitCodes = @(0)
+$targetFolder = Join-Path $(Get-BinRoot) $packageName
+Install-ChocolateyZipPackage "$packageName" "$url" "$targetFolder" "$url64"
+echo ("PHP installed in " + $targetFolder)
