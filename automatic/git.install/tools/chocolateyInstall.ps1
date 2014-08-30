9e285f7db4f893cc1a44f58da5efa1a09f62e36a
Rob Reynolds <ferventcoder@gmail.com>
2014-08-30 09:00:40 -0500

    (wip) git should get path from registry key

--- automatic/git.install/tools/chocolateyInstall.ps1
+++ automatic/git.install/tools/chocolateyInstall.ps1
@@ -2,6 +2,14 @@ try {
   Install-ChocolateyPackage 'git.install' 'exe' '/VERYSILENT /NORESTART /NOCANCEL /SP- /NOICONS  /COMPONENTS="assoc,assoc_sh" /LOG' '{{DownloadUrl}}'
 
   #------- ADDITIONAL SETUP -------#
+  #$uninstallKey = 'Git_is1'
+  #$key = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$uninstallKey"
+  #HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Git_is1
+  #HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Git_is1
+  #HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Git_is1
+  #$key = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Git_is1'
+  #Test-Path $key
+  #(Get-ItemProperty -Path $key -Name ProgramFilesDir).ProgramFilesDir
   $is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
   $programFiles = $env:programfiles
   if ($is64bit) {$programFiles = ${env:ProgramFiles(x86)}}
