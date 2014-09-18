ef8deef0b268aff515be87582673f6684be759cb
TomOne <TomOne@users.noreply.github.com>
2014-09-18 19:56:49 +0200

    Build dropbox package with enhanced description

---
+++ automatic/_output/dropbox/2.10.30.20140918/tools/chocolateyUninstall.ps1
@@ -0,0 +1,18 @@
+﻿try {
+
+    $packageName = 'dropbox'
+    $fileType = 'exe'
+    $silentArgs = '/S'
+    $uninstallerPath = Join-Path $env:APPDATA 'Dropbox\bin\DropboxUninstaller.exe'
+
+    if (Test-Path $uninstallerPath) {
+        Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $uninstallerPath
+    }
+
+    Write-ChocolateySuccess $packageName
+
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
+
