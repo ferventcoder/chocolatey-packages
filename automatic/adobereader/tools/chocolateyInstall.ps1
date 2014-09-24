5791899368e987f3982a5c3f5616f6a545c85b71
Rob Reynolds <ferventcoder@gmail.com>
2014-09-24 08:11:38 -0500

    Adobe reader multi language downloads

--- automatic/adobereader/tools/chocolateyInstall.ps1
+++ automatic/adobereader/tools/chocolateyInstall.ps1
@@ -1,8 +1,31 @@
-﻿$silentArgs = '/sPB /msi /norestart /quiet ALLUSERS=1 EULA_ACCEPT=YES'
+﻿$packageName = 'adobereader'
+$installerType = 'EXE'
+$silentArgs = '/sAll /msi /norestart /quiet ALLUSERS=1 EULA_ACCEPT=YES' #/sPB
+#$url = 'http://ardownload.adobe.com/pub/adobe/reader/win/{mainversion}.x/{version}/en_US/AdbeRdr{version:replace:.:}_en_US.exe'
 $downloadUrl = '{{DownloadUrl}}'
 #http://forums.adobe.com/thread/754256
 #http://www.appdeploy.com/messageboards/tm.asp?m=37416
 # '/sPB /msi /norestart ALLUSERS=1 EULA_ACCEPT=YES'
 #'/sAll /rs /msi "/qb-! /norestart ALLUSERS=1 EULA_ACCEPT=YES SUPPRESS_APP_LAUNCH=YES"'
 #'/msi /norestart /quiet'
-Install-ChocolateyPackage 'adobereader' 'exe' "$silentArgs" "$downloadUrl"
+
+
+
+# the url changes based on the language - not entirely happy with the way this works
+$LCID = (Get-Culture).LCID
+if(($LCID -eq "3082") -or ($LCID -eq "1034")){ ## Spanish
+  $url = $url -replace 'en_US', 'es_ES'
+} elseif($LCID -eq "1036"){ ## French
+  $url = $url -replace 'en_US', 'fr_FR'
+} elseif($LCID -eq "1031"){ ## German
+  $url = $url -replace 'en_US', 'de_DE'
+} elseif($LCID -eq "1041"){ ## Japanese
+  $url = $url -replace 'en_US', 'ja_JP'
+} else{ ## English
+  $url = $url
+}
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
+
+#LCID table
+#http://msdn.microsoft.com/es-es/goglobal/bb964664.aspx
