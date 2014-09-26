6ddd92dbac4540903f011f11fd8f66a21ad604ee
TomOne <TomOne@users.noreply.github.com>
2014-09-26 23:46:07 +0200

    Convert anyvideoconverter to automatic package

--- automatic/anyvideoconverter/tools/chocolateyInstall.ps1
+++ automatic/anyvideoconverter/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'anyvideoconverter'
 $installerType = 'exe'
-$url = 'http://www.any-video-converter.com/avc-free.exe'
+$url = '{{DownloadUrl}}'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
