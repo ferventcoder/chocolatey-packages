22f1e88a116bb70f989e0891bbc87dde14ee4a86
TomOne <TomOne@users.noreply.github.com>
2014-09-27 02:35:52 +0200

    Convert pspad to automatic package

--- automatic/pspad/tools/chocolateyInstall.ps1
+++ automatic/pspad/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $packageName = 'pspad'
 $installerType = 'exe'
-$url = 'http://pspad.siliconhill.cz/files/pspad457inst_en.exe'
+$url = '{{DownloadUrl}}'
 $silentArgs = ' /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
