8b8676be3d9ed680142469a342380a55091adcbc
TomOne <TomOne@users.noreply.github.com>
2014-09-27 18:47:30 +0200

    CloudBerryExplorer.S3 to auto package

--- automatic/CloudBerryExplorer.S3/tools/chocolateyInstall.ps1
+++ automatic/CloudBerryExplorer.S3/tools/chocolateyInstall.ps1
@@ -1,9 +1,8 @@
 ﻿$packageName = 'CloudBerryExplorer.S3'
 $installerType = 'exe'
-$url = 'http://www.cloudberrylab.com/download/CloudBerryExplorerSetup_v3.7.0.31.exe'
+$url = '{{DownloadUrl}}'
 $url64 = $url
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
-
