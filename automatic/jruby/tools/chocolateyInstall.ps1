d35c3f4a55bd49763b949f3f91bf1621b6df73de
TomOne <TomOne@users.noreply.github.com>
2014-09-27 19:05:14 +0200

    Convert jruby to auto package

--- automatic/jruby/tools/chocolateyInstall.ps1
+++ automatic/jruby/tools/chocolateyInstall.ps1
@@ -1,9 +1,8 @@
 ﻿$packageName = 'jruby'
 $installerType = 'exe'
-$url = 'http://jruby.org.s3.amazonaws.com/downloads/1.7.10/jruby_windows_1_7_10.exe'
-$url64 = 'http://jruby.org.s3.amazonaws.com/downloads/1.7.10/jruby_windows_x64_1_7_10.exe'
+$url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
 $silentArgs = '-q'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
-
