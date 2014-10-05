b3359cb09bb1d42b9e6a4a377128513ebceb6f71
TomOne <TomOne@users.noreply.github.com>
2014-10-05 11:11:40 +0200

    Bump ruby package to v2.1.3 (taken from Gallery)

--- manual/ruby/tools/chocolateyInstall.ps1
+++ manual/ruby/tools/chocolateyInstall.ps1
@@ -14,18 +14,28 @@
   # $rubyFolder = '187'
   # $url = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-1.8.7-p374.exe?direct'
 
+  # $checksum = '2e33a098f126275f7cb29ddcd0eb9845'
+
   # $rubyFolder = '193'
   # $url = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-1.9.3-p545.exe?direct'
-  # $url64 = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-1.9.3-p545.exe?direct'
+  # $checksum = '05398a6cd920ccd297c28150a935ef72'
 
-  $rubyFolder = '200'
-  $url = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.0.0-p481.exe?direct'
-  $url64 = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.0.0-p481-x64.exe?direct'
+  # $rubyFolder = '200'
+  # $url = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.0.0-p576.exe?direct'
+  # $checksum = '723ae8cda24a86c16914582a340deece'
+  # $url64 = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.0.0-p576-x64.exe?direct'
+  # $checksum64 = 'a6ab3963c571055cba649237d3f4a771'
+
+  $rubyFolder = '213'
+  $url = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.1.3.exe?direct'
+  $checksum = 'e5e2c028d76895e9da6c3c86965fc747'
+  $url64 = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.1.3-x64.exe?direct'
+  $checksum64 = '0f29131a6852cf92382c082eaf1dfd34'
 
   $rubyPath = join-path $binRoot $('ruby' + "$rubyFolder")
   $silentArgs = "/verysilent /dir=`"$rubyPath`" /tasks=`"assocfiles,modpath`""
 
-  Install-ChocolateyPackage 'ruby' 'exe' "$silentArgs" "$url" "$url64"
+  Install-ChocolateyPackage 'ruby' 'exe' "$silentArgs" "$url" "$url64" #-checksum $checksum -checksum64 $checksum64
 
   $rubyBin = join-path $rubyPath 'bin'
   Write-Host "Adding `'$rubyBin`' to the local path"
@@ -47,4 +57,3 @@
   Write-ChocolateyFailure 'ruby' $($_.Exception.Message)
   throw
 }
-
