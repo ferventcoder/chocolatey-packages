a6d92a389ee14b74af2c9a5f0ec262c99ff3cec0
TomOne <TomOne@users.noreply.github.com>
2014-10-05 12:36:41 +0200

    Bump ruby.portable package to v2.1.3 (taken from Gallery)

--- manual/ruby.portable/tools/chocolateyInstall.ps1
+++ manual/ruby.portable/tools/chocolateyInstall.ps1
@@ -1,15 +1,25 @@
 ﻿$packageName = 'ruby.portable'
+
+# 1.8.7
 #$url = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-1.8.7-p374-i386-mingw32.7z?direct'
-
 #$checksum = '2eabeb3bb210d088083c0c04fdf94c7e'
-$url = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-1.9.3-p545-i386-mingw32.7z?direct'
-$checksum = '8e7f07256c86bfd6fc73fbcc13db4b09'
-#$url = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.0.0-p481-i386-mingw32.7z?direct'
 
-#$checksum = '1a43404393f080e0225509415866307b'
-#$url64 = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.0.0-p481-x64-mingw32.7z?direct'
+# 1.9.3
+# $url = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-1.9.3-p545-i386-mingw32.7z?direct'
+# $checksum = '8e7f07256c86bfd6fc73fbcc13db4b09'
 
-#$checksum64 = '61a3f9d707ea3bf5af57de29d463b191'
+
+# 2.0.0
+# $url = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.0.0-p576-i386-mingw32.7z?direct'
+# $checksum = '40a87864ca89bbd46f8083bb5cd52d89'
+# $url64 = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.0.0-p576-x64-mingw32.7z?direct'
+# $checksum64 = 'a097cb750316e8bfa25fd836dd4296fa'
+
+# 2.1.3
+$url = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.1.3-i386-mingw32.7z?direct'
+$checksum = '60e39aaab140c3a22abdc04ec2017968'
+$url64 = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.1.3-x64-mingw32.7z?direct'
+$checksum64 = 'd339f956db4d4b1e8a30ac3e33014844'
 
 try {
   $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
@@ -17,8 +27,8 @@ try {
 
 
   $file = Join-Path $toolsDir "$($packageName).7z"
-  Get-ChocolateyWebFile "$packageName" "$file" "$url" -checksum "$checksum"
-  #Get-ChocolateyWebFile "$packageName" "$file" "$url" "$url64" -checksum "$checksum" -checksum64 "$checksum64"
+  #Get-ChocolateyWebFile "$packageName" "$file" "$url" -checksum "$checksum"
+  Get-ChocolateyWebFile "$packageName" "$file" "$url" "$url64" -checksum "$checksum" -checksum64 "$checksum64"
 
   Get-ChocolateyUnzip "$file" "$installDir" -packageName "$packageName"
 
@@ -27,4 +37,3 @@ try {
   Write-ChocolateyFailure $packageName $($_.Exception.Message)
   throw
 }
-
