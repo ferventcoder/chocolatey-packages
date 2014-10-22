ffa38e604da174b8cb796a3ba4d3cb8b51bda74e
Rob Reynolds <ferventcoder@gmail.com>
2014-10-22 05:40:06 -0500

    updates prior to automatic run for 20141022_054000

---
+++ automatic/_output/1password/4.1.0.526/tools/chocolateyInstall.ps1
@@ -0,0 +1,2 @@
+﻿Install-ChocolateyPackage '1password' 'exe' '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 'https://d13itkw33a7sus.cloudfront.net/dist/1P/win4/1Password-4.1.0.526.exe' -validExitCodes @(0)
+
