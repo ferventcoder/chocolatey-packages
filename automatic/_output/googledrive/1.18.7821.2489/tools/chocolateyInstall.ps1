0a43151ae048b2ab2a3c9724805fe77ab185b97c
Rob Reynolds <ferventcoder@gmail.com>
2014-11-01 05:40:06 -0500

    updates prior to automatic run for 20141101_054000

---
+++ automatic/_output/googledrive/1.18.7821.2489/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿Install-ChocolateyPackage 'googledrive' 'msi' '/quiet' 'https://dl.google.com/drive/1.18.7821.2489/gsync.msi' -validExitCodes @(0,3010)
+
+# One user suggested https://dl.google.com/drive/gsync_enterprise.msi
+
