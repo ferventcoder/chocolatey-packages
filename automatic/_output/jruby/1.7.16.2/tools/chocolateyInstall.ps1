e0dfc83adb146321d508ab407ecbd7c514169803
Rob Reynolds <ferventcoder@gmail.com>
2014-12-11 05:40:07 -0600

    updates prior to automatic run for 20141211_054000

---
+++ automatic/_output/jruby/1.7.16.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'jruby'
+$installerType = 'exe'
+$url = 'http://jruby.org.s3.amazonaws.com/downloads/1.7.16.2/jruby_windows_1_7_16_2.exe'
+$url64 = 'http://jruby.org.s3.amazonaws.com/downloads/1.7.16.2/jruby_windows_x64_1_7_16_2.exe'
+$silentArgs = '-q'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
