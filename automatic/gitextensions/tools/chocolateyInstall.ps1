374445d3c175371635ec28bbc168758962710c6f
TomOne <TomOne@users.noreply.github.com>
2014-12-14 01:56:47 +0100

    Minor improvements for gitextensions package
    * More readable installer script
    * Make packageId mor DRY
    * Use Chocolatey's Get-ProcessorBits function

--- automatic/gitextensions/tools/chocolateyInstall.ps1
+++ automatic/gitextensions/tools/chocolateyInstall.ps1
@@ -1,20 +1,28 @@
 ﻿try {
-  # \{\{DownloadUrlx64\}\} is the required variable
-  Install-ChocolateyPackage 'gitextensions' 'msi' '/quiet' '{{DownloadUrlx64}}'
+
+  $packageId = '{{PackageName}}'
+  $installerType = 'msi'
+  $installArgs = '/quiet /norestart'
+  # In case you see \{\{DownloadUrlx64\}\} (without backslashes)
+  # after the commented lines, it’s intended.
+  $url = '{{DownloadUrlx64}}'
+
+  Install-ChocolateyPackage "$packageId" "$installerType" "$installArgs" "$url"
 
   #------- ADDITIONAL SETUP -------#
-  $processor = Get-WmiObject Win32_Processor
-  $is64bit = $processor.AddressWidth -eq 64
+  $osBitness = Get-ProcessorBits
+  $is64bit = $osBitness -eq 64
 
   $progFiles = [System.Environment]::GetFolderPath('ProgramFiles')
   if ($is64bit -and $progFiles -notmatch 'x86') {$progFiles = "$progFiles (x86)"}
 
-  $gitexPath = Join-Path $progFiles "GitExtensions"
+  $gitexPath = Join-Path $progFiles 'GitExtensions'
   Write-Host "Adding `'$gitexPath`' to the PATH so you can call gitex from the command line."
   Install-ChocolateyPath $gitexPath
+  $env:Path = "$($env:Path);$gitexPath"
 
-  Write-ChocolateySuccess 'gitextensions'
+  Write-ChocolateySuccess "$packageId"
 } catch {
-  Write-ChocolateyFailure 'gitextensions' "$($_.Exception.Message)"
+  Write-ChocolateyFailure "$packageId" "$($_.Exception.Message)"
   throw
 }
