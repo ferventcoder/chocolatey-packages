7dcb7b34ef9cdc3ded35399baccd32306ec13d92
Patrick <patrickmaurer@users.noreply.github.com>
2014-12-17 21:54:52 +0100

    Make package silent and add valid exit codes

--- manual/ie9/tools/chocolateyInstall.ps1
+++ manual/ie9/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-﻿#auto installers
-#$urlToWin7_32bit = "http://view.atdmt.com/action/UMIRF_IE_IE9HP_Win7?href=http://download.microsoft.com/download/8/6/D/86DB5DC9-5706-4A5B-BD46-FFBA6FA67D44/IE9-Windows7-x86-enu.exe"
-#$urlToWin7_64bit = "http://view.atdmt.com/action/UMIRF_IE_IE9HP_Win7?href=http://download.microsoft.com/download/8/6/D/86DB5DC9-5706-4A5B-BD46-FFBA6FA67D44/IE9-Windows7-x64-enu.exe"
-#downloads the whole thing and gives you the option of no.
-$urlToWin7_32bit = "http://view.atdmt.com/action/UMIRF_IE_IE9WW_InternationalDL?href=http://download.microsoft.com/download/C/3/B/C3BF2EF4-E764-430C-BDCE-479F2142FC81/IE9-Windows7-x86-enu.exe"
-$urlToWin7_64bit = "http://view.atdmt.com/action/UMIRF_IE_IE9WW_InternationalDL?href=http://download.microsoft.com/download/C/1/6/C167B427-722E-4665-9A40-A37BC5222B0A/IE9-Windows7-x64-enu.exe"
-Install-ChocolateyPackage 'IE9' 'exe' '' "$urlToWin7_32bit" "$urlToWin7_64bit"
-
+﻿$packageName = 'ie9'
+$installerType = 'EXE'
+$32BitUrl = "http://view.atdmt.com/action/UMIRF_IE_IE9WW_InternationalDL?href=http://download.microsoft.com/download/C/3/B/C3BF2EF4-E764-430C-BDCE-479F2142FC81/IE9-Windows7-x86-enu.exe"
+$64BitUrl = "http://view.atdmt.com/action/UMIRF_IE_IE9WW_InternationalDL?href=http://download.microsoft.com/download/C/1/6/C167B427-722E-4665-9A40-A37BC5222B0A/IE9-Windows7-x64-enu.exe"
+$silentArgs = '/Passive /NoRestart'
+$validExitCodes = @(0,3010,40013) #3010: reboot required, 40013: already installed
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" "$64BitUrl" -validExitCodes $validExitCodes
+Write-Host 'You will likely need to restart your computer for the install to take effect.' -ForeGroundColor green
