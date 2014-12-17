1acf9bc12bfbbb862cdd2f5741d416cf7dbb8986
Patrick <patrickmaurer@users.noreply.github.com>
2014-12-17 22:01:32 +0100

    Sorten URLs

--- manual/ie9/tools/chocolateyInstall.ps1
+++ manual/ie9/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'ie9'
 $installerType = 'EXE'
-$32BitUrl = "http://view.atdmt.com/action/UMIRF_IE_IE9WW_InternationalDL?href=http://download.microsoft.com/download/C/3/B/C3BF2EF4-E764-430C-BDCE-479F2142FC81/IE9-Windows7-x86-enu.exe"
-$64BitUrl = "http://view.atdmt.com/action/UMIRF_IE_IE9WW_InternationalDL?href=http://download.microsoft.com/download/C/1/6/C167B427-722E-4665-9A40-A37BC5222B0A/IE9-Windows7-x64-enu.exe"
+$32BitUrl = 'http://download.microsoft.com/download/C/3/B/C3BF2EF4-E764-430C-BDCE-479F2142FC81/IE9-Windows7-x86-enu.exe'
+$64BitUrl = 'http://download.microsoft.com/download/C/1/6/C167B427-722E-4665-9A40-A37BC5222B0A/IE9-Windows7-x64-enu.exe'
 $silentArgs = '/Passive /NoRestart'
 $validExitCodes = @(0,3010,40013) #3010: reboot required, 40013: already installed
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" "$64BitUrl" -validExitCodes $validExitCodes
