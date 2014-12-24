7188429f616dec7996890abc5fb49170aeac528b
TomOne <TomOne@users.noreply.github.com>
2014-12-24 15:10:01 +0100

    miranda downloads now hosted on SF. Fix ketarin job

--- automatic/miranda/tools/chocolateyInstall.ps1
+++ automatic/miranda/tools/chocolateyInstall.ps1
@@ -1,2 +1,9 @@
-﻿Install-ChocolateyPackage '{{PackageName}}' 'exe' '/S' '{{DownloadUrl}}' -validExitCodes @(0)
+﻿$packageId = '{{PackageName}}'
+$fileType = 'exe'
+$fileArgs = '/S'
 
+# If you see \{\{DownloadUrlx64\}\} (without backslashes) in the source code,
+# it’s intentional.
+$url = '{{DownloadUrlx64}}'
+
+Install-ChocolateyPackage $packageId $fileType $fileArgs $url -validExitCodes @(0)
