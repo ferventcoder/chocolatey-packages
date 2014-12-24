8af797ce46fbbe4589bc169e059bbd8de9291e9b
TomOne <TomOne@users.noreply.github.com>
2014-12-24 16:00:55 +0100

    vim package: clean up code, replace custom functions with those baked into Chocolatey

--- automatic/vim/tools/chocolateyInstall.ps1
+++ automatic/vim/tools/chocolateyInstall.ps1
@@ -1,13 +1,40 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$args = '/S'
-$version = '{{PackageVersion}}'
-$versionDash = $version -replace '\.', '-'
+﻿try {
+  $packageName = '{{PackageName}}'
+  $fileType = 'exe'
+  $args = '/S'
+  $version = '{{PackageVersion}}'
+  $versionDash = $version -replace '\.', '-'
 
-$url = "http://sourceforge.net/projects/cream/files/Vim/${version}/gvim-${versionDash}.exe/download"
+  $url = "http://sourceforge.net/projects/cream/files/Vim/${version}/gvim-${versionDash}.exe/download"
 
-Install-ChocolateyPackage $packageName $fileType $args $url
+  Install-ChocolateyPackage $packageName $fileType $args $url
 
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+  # Get installation directory from registry
 
-Start-ChocolateyProcessAsAdmin (Join-Path $scriptDir 'chocolateyPostInstallUAC.ps1')
+  $regPathWow6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Vim'
+  $regPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Vim'
+
+  if (Test-Path $regPath) {
+    $regPathFound = $regPath
+  }
+
+  if (Test-Path $regPathWow6432) {
+    $regPathFound = $regPathWow6432
+  }
+
+  if ($regPathFound) {
+    # Add the installation directory to PATH
+    $installDir = Split-Path -Parent (
+      Get-ItemProperty $regPathFound 'UninstallString'
+    ).UninstallString
+
+    Write-Host 'Adding the vim installation directory to PATH …'
+    Install-ChocolateyPath $installDir 'Machine'
+    $env:Path = "$($env:Path);$installDir"
+  }
+
+
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
