c75529fab8a006c30c864d11d210a0c91d197737
TomOne <TomOne@users.noreply.github.com>
2014-12-24 14:39:46 +0100

    Add commented lines with URLs and checksums for older ruby versions

--- manual/ruby/tools/chocolateyInstall.ps1
+++ manual/ruby/tools/chocolateyInstall.ps1
@@ -3,6 +3,21 @@
   $packageId = 'ruby'
   $binRoot = Get-BinRoot
 
+  # $rubyFolder = '187'
+  # $url = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-1.8.7-p374.exe?direct'
+
+  # $checksum = '2e33a098f126275f7cb29ddcd0eb9845'
+
+  # $rubyFolder = '193'
+  # $url = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-1.9.3-p545.exe?direct'
+  # $checksum = '05398a6cd920ccd297c28150a935ef72'
+
+  # $rubyFolder = '200'
+  # $url = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.0.0-p576.exe?direct'
+  # $checksum = '723ae8cda24a86c16914582a340deece'
+  # $url64 = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.0.0-p576-x64.exe?direct'
+  # $checksum64 = 'a6ab3963c571055cba649237d3f4a771'
+
   $rubyFolder = '215'
   $url = 'http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.1.5.exe?direct'
   $checksum = 'eacd2526ef61fb73c0e642828675e94d'
