3c08d6f940992aecced0d2bab632fba0291492b7
TomOne <TomOne@users.noreply.github.com>
2014-12-26 00:02:25 +0100

    git.commandline: Clean up install script, make it more DRY, remove obsolete 7zip dep, use Get-BinRoot

--- automatic/git.commandline/tools/chocolateyInstall.ps1
+++ automatic/git.commandline/tools/chocolateyInstall.ps1
@@ -1,38 +1,51 @@
 ﻿try {
-  $binRoot = "$env:systemdrive\"
 
-  ### Using an environment variable to to define the bin root until we implement YAML configuration ###
-  if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
-  $gitInstallDir = join-path $binRoot 'git'
+  $packageId = '{{PackageName}}'
 
-  write-host "Chocolatey is installing git to $gitInstallDir"
-  write-host "Please wait..."
-  Start-sleep 3
+  $binRoot = Get-BinRoot
+  $gitInstallDir = Join-Path $binRoot 'git'
 
-  if (![System.IO.Directory]::Exists($gitInstallDir)) {[System.IO.Directory]::CreateDirectory($gitInstallDir)}
-  $tempDir = "$env:TEMP\chocolatey\git.commandline"
-  if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
-  $file = Join-Path $tempDir "git.7z"
-  #Get-ChocolateyWebFile 'git' "$file" 'http://msysgit.googlecode.com/files/PortableGit-1.7.10-preview20120409.7z'
-  Get-ChocolateyWebFile 'git' "$file" '{{DownloadUrl}}'
+  $deprecatedInstallDir = Join-Path $env:systemdrive 'git'
 
-  write-host "Cleaning out the contents of $gitInstallDir"
-  Remove-Item "$($gitInstallDir)\*" -recurse -force
+  if (Test-Path $deprecatedInstallDir) {
 
-  write-host "Extracting the contents of $file to $gitInstallDir"
-  #& 7za x -o"$gitInstallDir" -y "$file"
-  Start-Process "7za" -ArgumentList "x -o`"$gitInstallDir`" -y `"$file`"" -Wait
+    $gitInstallDir = $deprecatedInstallDir
+
+    Write-Host $(
+      "Warning: Deprecated installation directory detected ('$deprecatedInstallDir'). " +
+      "This package will continue to install there. If you want this message to " +
+      "disappear, move the installation location to '$gitInstallDir' " +
+      "and force install this package again."
+    )
+  }
+
+  Write-Host "Chocolatey is installing git to '$gitInstallDir'"
+
+  $url = '{{DownloadUrl}}'
+
+  if (Test-Path $gitInstallDir) {
+    Write-Host "Cleaning out the contents of '$gitInstallDir'"
+    Remove-Item "$($gitInstallDir)\*" -Recurse -Force
+  }
+
+  Write-Host "Extracting the contents of Git Portable to '$gitInstallDir'"
+
+  Install-ChocolateyZipPackage $packageId $url $gitInstallDir
 
   Install-ChocolateyPath $gitInstallDir 'user'
+  $env:Path = "$($env:Path);$gitInstallDir"
+
   Install-ChocolateyPath "$($gitInstallDir)\cmd" 'user'
+  $env:Path = "$($env:Path);$($gitInstallDir)\cmd"
 
 #  Write-Host 'Making GIT core.autocrlf false'
 #  #make GIT core.autocrlf false
 #  & "$env:comspec" '/c git config --global core.autocrlf false'
 
-  Write-ChocolateySuccess 'git.commandline'
+  Write-ChocolateySuccess $packageId
+
 } catch {
-  Write-ChocolateyFailure 'git.commandline' $($_.Exception.Message)
+  Write-ChocolateyFailure $packageId $($_.Exception.Message)
   throw
 }
 
