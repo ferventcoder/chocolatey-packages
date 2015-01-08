78956db08eff719277a71212b3fd2eb7e1c5a398
Rob Reynolds <ferventcoder@gmail.com>
2015-01-08 12:23:11 -0600

    ruby.portable 2.0.0-p598

--- manual/ruby.portable/tools/chocolateyInstall.ps1
+++ manual/ruby.portable/tools/chocolateyInstall.ps1
@@ -5,14 +5,14 @@
 #$checksum = '2eabeb3bb210d088083c0c04fdf94c7e'
 
 # 1.9.3
-$url = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-1.9.3-p551-i386-mingw32.7z?direct'
-$checksum = '73ba6e292d3afec5cecc68ec64fd85bf'
+# $url = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-1.9.3-p551-i386-mingw32.7z?direct'
+# $checksum = '73ba6e292d3afec5cecc68ec64fd85bf'
 
 # 2.0.0
-# $url = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.0.0-p576-i386-mingw32.7z?direct'
-# $checksum = '40a87864ca89bbd46f8083bb5cd52d89'
-# $url64 = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.0.0-p576-x64-mingw32.7z?direct'
-# $checksum64 = 'a097cb750316e8bfa25fd836dd4296fa'
+$url = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.0.0-p598-i386-mingw32.7z?direct'
+$checksum = 'a4b7f5c3d3286b51ea35d34fde8aa174'
+$url64 = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.0.0-p598-x64-mingw32.7z?direct'
+$checksum64 = '011b3832b6e557e7b25fd98821e28e69'
 
 # 2.1.5
 #$url = 'http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.1.5-i386-mingw32.7z?direct'
@@ -26,8 +26,8 @@ try {
 
 
   $file = Join-Path $toolsDir "$($packageName).7z"
-  Get-ChocolateyWebFile "$packageName" "$file" "$url" -checksum "$checksum"
-  #Get-ChocolateyWebFile "$packageName" "$file" "$url" "$url64" -checksum "$checksum" -checksum64 "$checksum64"
+  #Get-ChocolateyWebFile "$packageName" "$file" "$url" -checksum "$checksum"
+  Get-ChocolateyWebFile "$packageName" "$file" "$url" "$url64" -checksum "$checksum" -checksum64 "$checksum64"
 
   Get-ChocolateyUnzip "$file" "$installDir" -packageName "$packageName"
 
