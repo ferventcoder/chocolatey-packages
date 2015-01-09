b3d5c01b9ad578a5215ab7d480a4fc306af4295c
Rob Reynolds <ferventcoder@gmail.com>
2015-01-09 05:40:11 -0600

    updates prior to automatic run for 20150109_054004

---
+++ automatic/_output/filezilla/3.10.0/tools/chocolateyUninstall.ps1
@@ -0,0 +1,32 @@
+﻿try {
+
+  $packageId = 'filezilla'
+
+  Write-ChocolateySuccess $packageId
+
+  $regPathWow6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\FileZilla Client'
+  $regPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FileZilla Client'
+
+  if (Test-Path $regPathWow6432) {
+    $existentRegPath = $regPathWow6432
+  }
+
+  if (Test-Path $regPath) {
+    $existentRegPath = $regPath
+  }
+
+  if ($existentRegPath) {
+
+    $uninstallString = (Get-ItemProperty $existentRegPath).UninstallString
+
+    if ($uninstallString) {
+      Uninstall-ChocolateyPackage $packageId 'exe' '/S' $uninstallString
+    }
+
+  }
+
+} catch {
+
+  Write-ChocolateyFailure $packageId $($_.Exception.Message)
+  throw
+}
