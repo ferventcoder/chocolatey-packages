a5e4e485f9c1106a76d1528d2652083a9f20bdfd
Rob Reynolds <ferventcoder@gmail.com>
2015-01-12 05:40:08 -0600

    updates prior to automatic run for 20150112_054000

---
+++ automatic/_output/notepadplusplus.commandline/6.7.4/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyZipPackage 'notepadplusplus.commandline' 'http://download.tuxfamily.org/notepadplus/6.7.4/npp.6.7.4.bin.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
---
+++ automatic/_output/notepadplusplus.install/6.7.4/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage 'notepadplusplus' 'exe' '/S' 'http://download.tuxfamily.org/notepadplus/6.7.4/npp.6.7.4.Installer.exe'
