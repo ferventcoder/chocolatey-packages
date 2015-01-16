6bef8614a734181a167a5471fea189f1224caf7b
TomOne <TomOne@users.noreply.github.com>
2015-01-16 19:44:51 +0100

    Split function parameters into variables

--- automatic/googledrive/tools/chocolateyInstall.ps1
+++ automatic/googledrive/tools/chocolateyInstall.ps1
@@ -1,4 +1,9 @@
-﻿Install-ChocolateyPackage 'googledrive' 'msi' '/quiet' 'https://dl.google.com/drive/{{PackageVersion}}/gsync.msi' -validExitCodes @(0,3010)
+﻿$packageName = 'googledrive'
+$fileType = 'msi'
+$silentArgs = '/quiet'
+$url = 'https://dl.google.com/drive/{{PackageVersion}}/gsync.msi'
+$validExitCodes = @(0, 3010)
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url -validExitCodes $validExitCodes
 
 # One user suggested https://dl.google.com/drive/gsync_enterprise.msi
-
