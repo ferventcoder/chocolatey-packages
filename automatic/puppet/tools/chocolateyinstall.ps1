d259efcb3a54b2f7f4012bbdc864e02ff95be9e8
TomOne <TomOne@users.noreply.github.com>
2015-01-17 19:56:26 +0100

    Improve puppet packages
    * Typographical corrections
    * Split function parameters into variables
    * Better icon

--- automatic/puppet/tools/chocolateyinstall.ps1
+++ automatic/puppet/tools/chocolateyinstall.ps1
@@ -1,3 +1,7 @@
-﻿Install-ChocolateyPackage 'puppet' 'MSI' '/qn' 'http://downloads.puppetlabs.com/windows/puppet-{{PackageVersion}}.msi' 'http://downloads.puppetlabs.com/windows/puppet-{{PackageVersion}}-x64.msi' -validExitCodes @(0)
+﻿$packageName = 'puppet'
+$fileType = 'MSI'
+$silentArgs = '/qn'
+$url = 'http://downloads.puppetlabs.com/windows/puppet-{{PackageVersion}}.msi'
+$url64 = 'http://downloads.puppetlabs.com/windows/puppet-{{PackageVersion}}-x64.msi'
 
-
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64 -validExitCodes @(0)
