18b9d0a6428f17fa6abfc93cbc1e6bcd5d508c5c
Rob Reynolds <ferventcoder@gmail.com>
2015-02-19 17:15:04 -0600

    putty and phantomjs changes

--- automatic/_output/PhantomJS/2.0.0/tools/chocolateyInstall.ps1
+++ automatic/_output/PhantomJS/2.0.0/tools/chocolateyInstall.ps1
@@ -1,30 +1,6 @@
-﻿try {
-  $package = 'PhantomJS'
-  $version = '2.0.0'
+﻿$package = 'PhantomJS'
+$version = '2.0.0'
+$zipUrl = 'https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.0.0-windows.zip'
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  if (!(Get-Command Get-BinRoot -errorAction SilentlyContinue))
-  {
-    Import-Module "$($installDir)\Get-BinRoot.ps1"
-  }
-
-
-  # Get $binRoot until we implement YAML configuration
-  $binRoot = Get-BinRoot
-
-  $installDir = Join-Path $binRoot $package
-  Write-Host "Adding `'$installDir`' to the path and the current shell path"
-  Install-ChocolateyPath "$installDir"
-  $env:Path = "$($env:Path);$installDir"
-  $zipUrl = 'https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.0.0-windows.zip'
-
-  Install-ChocolateyZipPackage $package "$zipUrl" "$installDir"
-
-  Copy-Item "$($installDir)\phantomjs-$version-windows\*" "$installDir" -Force -Recurse
-
-  Write-ChocolateySuccess $package
-} catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
-  throw
-}
-
+Install-ChocolateyZipPackage $package "$zipUrl" "$installDir"
