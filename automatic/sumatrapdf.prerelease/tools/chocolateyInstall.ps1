0a8b6f659799e103efe3a0b66f3351ba394dcbef
aronovgj <aronovgj@gmx.net>
2015-03-14 14:21:28 +0100

    update sumatrapdf
    update sumatrapdf

--- automatic/sumatrapdf.prerelease/tools/chocolateyInstall.ps1
+++ automatic/sumatrapdf.prerelease/tools/chocolateyInstall.ps1
@@ -1,2 +1,6 @@
-﻿Install-ChocolateyPackage '{{PackageName}}' 'exe' '/S' '{{DownloadUrl}}' -validExitCodes @(0)
-
+﻿$ProcessActive = Get-Process sumatraPDF* -ErrorAction SilentlyContinue
+if($ProcessActive -ne $null)
+{
+    Stop-Process -ProcessName sumatraPDF*
+}
+Install-ChocolateyPackage '{{PackageName}}' 'exe' '/S' '{{DownloadUrl}}' -validExitCodes @(0)
