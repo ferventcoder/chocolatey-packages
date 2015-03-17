8dd062c1ec385d505d5c5e6dd100ea96beeb2a58
Rob Reynolds <ferventcoder@gmail.com>
2015-03-17 05:40:07 -0500

    updates prior to automatic run for 20150317_054000

---
+++ automatic/_output/httrack.tool/3.48.21/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿try {
+
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  ### For BinRoot, use the following instead ###
+  #$binRoot = "$env:systemdrive\"
+  ### Using an environment variable to to define the bin root until we implement configuration ###
+  #if($env:chocolatey_bin_root -ne $null){$binRoot = join-path $env:systemdrive $env:chocolatey_bin_root}
+  #$installDir = Join-Path $binRoot 'httrack'
+  #Write-Host "Adding `'$installDir`' to the path and the current shell path"
+  #Install-ChocolateyPath "$installDir"
+  #$env:Path = "$($env:Path);$installDir"
+  $zipUrl = 'http://download.httrack.com/httrack-noinst-3.48.21.zip'
+  $zipUrl64 = 'http://download.httrack.com/httrack_x64-noinst-3.48.21.zip'
+
+  Install-ChocolateyZipPackage 'httrack.tool' "$zipUrl" "$installDir" "$zipUrl64"
+
+  Write-ChocolateySuccess 'httrack.tool'
+} catch {
+  Write-ChocolateyFailure 'httrack.tool' "$($_.Exception.Message)"
+  throw
+}
