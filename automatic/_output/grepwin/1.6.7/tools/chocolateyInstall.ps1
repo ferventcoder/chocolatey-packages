802914ef98fd33fc3a5863e54fcabdfbad622c06
Rob Reynolds <ferventcoder@gmail.com>
2015-04-27 11:00:59 -0500

    GrepWin fixes - new location

--- automatic/_output/grepwin/1.6.7/tools/chocolateyInstall.ps1
+++
@@ -1,1 +0,0 @@
-﻿Install-ChocolateyPackage 'grepwin' 'msi' '/quiet' 'http://sourceforge.net/projects/stefanstools/files/grepWin/1.6.7/grepWin-1.6.7.msi/download' 'http://sourceforge.net/projects/stefanstools/files/grepWin/1.6.7/grepWin-1.6.7-64.msi/download'
