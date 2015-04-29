06f6f85cd2af60249ca568c416ee6060757b4075
Rob Reynolds <ferventcoder@gmail.com>
2015-04-29 17:02:40 -0500

    put git on path by default

--- automatic/git.install/tools/chocolateyInstall.ps1
+++ automatic/git.install/tools/chocolateyInstall.ps1
@@ -18,7 +18,8 @@ $noAutoCrlf = $false # this does nothing unless true
 
 # Now parse the packageParameters using good old regular expression
 if ($packageParameters) {
-    $match_pattern = "\/(?<option>([a-zA-Z]+)):(?<value>([`"'])?([a-zA-Z0-9- _\\:\.]+)([`"'])?)|\/(?<option>([a-zA-Z]+))"
+    $match_pattern = "\/(?<option>([a-zA-Z]+)):(?<value>([`"'])?([a-zA-Z0-9- _\\:\.]+)([`"'])?)|\/(?<option>([a-zA-Z]+))"  
+    #"
     $option_name = 'option'
     $value_name = 'value'
 
@@ -90,13 +91,13 @@ Install-ChocolateyPackage $packageId $fileType $fileArgs $url
 
 $keyNames = Get-ItemProperty -Path $installKey
 
-# if ($gitCmdOnly -eq $false -and $unixTools -eq $false) {
-# $installLocation = $keyNames.InstallLocation
-# if ($installLocation -ne '') {
-  # $gitPath = Join-Path $installLocation 'cmd'
-  # Install-ChocolateyPath $gitPath 'Machine'
-# }
-# }
+if ($gitCmdOnly -eq $false -and $unixTools -eq $false) {
+  $installLocation = $keyNames.InstallLocation
+  if ($installLocation -ne '') {
+    $gitPath = Join-Path $installLocation 'cmd'
+    Install-ChocolateyPath $gitPath 'Machine'
+  }
+}
 
 Write-Warning "Git installed - You may need to close and reopen your shell for PATH changes to take effect."
 
