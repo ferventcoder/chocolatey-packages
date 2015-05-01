32eb929ddca3576385a69a76153a62e848956736
Rob Reynolds <ferventcoder@gmail.com>
2015-05-01 07:37:39 -0500

    notepadplusplus fixes

---
+++ automatic/_output/notepadplusplus.commandline/6.7.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyZipPackage 'notepadplusplus.commandline' 'http://notepad-plus-plus.org/repository/6.x/6.7.7/npp.6.7.7.bin.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
---
+++ automatic/_output/notepadplusplus.install/6.7.7/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage 'notepadplusplus' 'exe' '/S' 'http://notepad-plus-plus.org/repository/6.x/6.7.7/npp.6.7.7.Installer.exe'
