bbb897b0058f9a305496382ab8834184b14b5a0a
aronovgj <aronovgj@gmx.net>
2015-05-25 01:10:09 +0200

    remove sumatra pre
    remove sumatra pre

--- automatic/sumatrapdf.prerelease/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$ProcessActive = Get-Process sumatraPDF* -ErrorAction SilentlyContinue
-if($ProcessActive -ne $null)
-{
-    Stop-Process -ProcessName sumatraPDF*
-}
-Install-ChocolateyPackage '{{PackageName}}' 'exe' '/S' '{{DownloadUrl}}' -validExitCodes @(0)
