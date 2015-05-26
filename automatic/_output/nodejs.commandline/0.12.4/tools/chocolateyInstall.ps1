7b94c139635a75857f0fc023376873ff54e9f438
Rob Reynolds <ferventcoder@gmail.com>
2015-05-26 05:40:04 -0500

    updates prior to automatic run for 20150526_054000

---
+++ automatic/_output/nodejs.commandline/0.12.4/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'nodejs'
+$url = 'http://nodejs.org/dist/v0.12.4/node.exe'
+$url64 = 'http://nodejs.org/dist/v0.12.4/x64/node.exe'
+
+try {
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+  $nodePath = Join-Path $installDir 'node.exe'
+  Get-ChocolateyWebFile "$packageName" "$nodePath" "$url" "$url64"
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
