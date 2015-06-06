6b4714cc64036cd55f39305ba1101e5577931bae
Rob Reynolds <ferventcoder@gmail.com>
2015-06-06 05:40:06 -0500

    updates prior to automatic run for 20150606_054000

---
+++ automatic/_output/postgresql.v9/9.4.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿try {
+  $toolsDir ="$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  Start-ChocolateyProcessAsAdmin "& $($toolsDir)\installpostgre.ps1"
+
+  Write-ChocolateySuccess 'postgresql'
+} catch {
+  Write-ChocolateyFailure 'postgresql' "$($_.Exception.Message)"
+  throw
+}
