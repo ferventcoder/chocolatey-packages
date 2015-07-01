cb14255deec9f7bf36c2d07be51d693a7ca88b16
Rob Reynolds <ferventcoder@gmail.com>
2015-07-01 07:14:44 -0500

    Fix foobar package

--- automatic/_output/foobar2000/1.3.8/tools/Get-FilenameFromRegex.ps1
+++ automatic/_output/foobar2000/1.3.8/tools/Get-FilenameFromRegex.ps1
@@ -11,39 +11,32 @@
 # Remember to escape regex characters, like the question mark in the querystring
 
 function Get-FilenameFromRegex {
-    param(
-        [string]$source_url,
-        [string]$find,
-        [string]$replace
-    )
+param(
+    [string]$source_url,
+    [string]$find,
+    [string]$replace
+)
+  ## Example # $source_url = "http://www.joejoesoft.com/vcms/hot/userupload/8/files/rmv303.zip"
+  ## Example # $find = '/cms/file.php\?f=userupload/8/files/rmv303.zip&amp;c=([\w\d]+)'
+  ## Example # $replace = 'http://www.joejoesoft.com/sim/$1/userupload/8/files/rmv303.zip'
 
-    try {
+  $ErrorActionPreference = "Stop"
+  
+  # Download and open download page
+  $wc = new-object system.net.webclient
+  $wc.UseDefaultCredentials = $true
+  $html = $wc.downloadstring($source_url)
 
-        ## Example # $source_url = "http://www.joejoesoft.com/vcms/hot/userupload/8/files/rmv303.zip"
-        ## Example # $find = '/cms/file.php\?f=userupload/8/files/rmv303.zip&amp;c=([\w\d]+)'
-        ## Example # $replace = 'http://www.joejoesoft.com/sim/$1/userupload/8/files/rmv303.zip'
+  $nothing =''+$html -cmatch $find
+  #if ($matches -eq $null) {
+  #  throw "No matches found"
+  #}
+  # Replace Match-references with previous matches: $1 $2 -> $matches[1] $matches[2]
+  #$download_url = $replace -creplace "\$`(\d+)",$matches[$1]
+  # This one is not working :(
+  # I have a temporary fix that only allows ONE match. However, in most cases we only need one match.
+  $download_url = $replace -creplace "\$`(\d+)",$matches[1]
 
-        # Download and open download page
-        $tempfile = "$env:TEMP\chocolatey_temp_download.html"
-        $wc = new-object system.net.webclient
-        $wc.UseDefaultCredentials = $true
-        $wc.downloadfile($source_url, $tempfile)
-        $source = Get-Content $tempfile
-
-        $nothing = ''+$source -cmatch $find # Get the matches, prevent output
-        # Replace Match-references with previous matches: $1 $2 -> $matches[1] $matches[2]
-        #$download_url = $replace -creplace "\$`(\d+)",$matches[$1]
-        # This one is not working :(
-        # I have a temporary fix that only allows ONE match. However, in most cases we only need one match.
-        $download_url = $replace -creplace "\$`(\d+)",$matches[1]
-        Remove-Item $tempfile
-
-        return $download_url
-
-    } catch {
-        $errorMessage = "Could not get Regex from download page."
-        Write-Error $errorMessage
-        throw $errorMessage
-  }
+  return $download_url
 }
 
