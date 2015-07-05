05d8bdecc57808ee6a379c1df1c0681a53993be8
Rob Reynolds <ferventcoder@gmail.com>
2015-07-05 05:40:04 -0500

    updates prior to automatic run for 20150705_054000

---
+++ automatic/_output/nodejs.install/0.12.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'nodejs.install'
+$installerType = 'msi'
+$url = 'http://nodejs.org/dist/v0.12.6/node-v0.12.6-x86.msi'
+$url64 = 'http://nodejs.org/dist/v0.12.6/x64/node-v0.12.6-x64.msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+
+$nodePath = "$env:SystemDrive\Program Files\nodejs"
+if (![System.IO.Directory]::Exists($nodePath)) {$nodePath = "$env:SystemDrive\Program Files (x86)\nodejs";}
+
+$env:Path = "$($env:Path);$nodePath"
