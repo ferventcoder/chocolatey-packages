18396168ca3d7d1b1569247f35f387ec063a297a
Rob Reynolds <ferventcoder@gmail.com>
2015-07-12 14:04:06 -0500

    vagrant auto-package

---
+++ automatic/vagrant/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = 'vagrant'
+$url = '{{DownloadUrl}}'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'msi'
+  url           = $url
+  silentArgs    = "/qn /norestart /l*v '$env:TEMP\chocolatey\$packageName.install.log'"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
