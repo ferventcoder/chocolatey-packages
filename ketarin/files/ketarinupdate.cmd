ae0148546da052427a3d3b521c38ed56646a478a
Chocolatey <chocolatey@realdimensions.net>
2015-07-11 22:12:32 -0700

    (ketarin) control job
    The steps to keep everything in sync are realized through automatically importing all the ketarin file changes every run.
    
    Update the github repo and sync it with master.
    
    Call ketarin with the database in source.

---
+++ ketarin/files/ketarinupdate.cmd
@@ -0,0 +1,23 @@
+@echo off
+
+SET DIR=%~dp0%
+
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
+For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
+
+::import all the files
+FOR /f "tokens=*" %%F IN ('dir %DIR%..\ /b *.ketarin.xml') DO (
+	call ketarin.exe /database="%DIR%\jobs.db" /import="%DIR%..\%%F"
+)
+
+echo Updating the github repo first
+pushd %DIR%
+call git add .
+call git commit -m "updates prior to automatic run for %mydate%_%mytime%"
+call git reset --hard HEAD
+call git fetch && git rebase origin/master
+call git push origin master
+popd
+
+echo Calling ketarin now the the repo has been updated
+call "Ketarin.exe" /silent /notify /database="%DIR%\jobs.db" /log=C:\ProgramData\chocolateypackageupdater\ketarin.%mydate%_%mytime%.log
