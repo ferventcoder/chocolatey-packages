366a92211835b96fa68387bf667da08891c1d030
Rob Reynolds <ferventcoder@gmail.com>
2015-07-13 16:38:00 -0500

    (ketarin) updater enhancements
    - ensure the repo is updated prior to the ketarin import
    - build in a small timeout for each import - at 500 packages it would
    take approx 17 minutes to run through every ketarin xml and import it.

--- ketarin/files/ketarinupdate.cmd
+++ ketarin/files/ketarinupdate.cmd
@@ -5,14 +5,6 @@ SET DIR=%~dp0%
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
 
-::import all the files
-FOR /f "tokens=*" %%F IN ('dir %DIR%..\ /b *.ketarin.xml') DO (
-	call ketarin.exe /database="%DIR%\jobs.db" /import="%DIR%..\%%F"
-)
-
-Echo Wait for all of those to finish importing...
-TIMEOUT /T 30
-
 echo Updating the github repo first
 pushd %DIR%
 call git add .
@@ -21,6 +13,15 @@ call git commit -m "updates prior to automatic run for %mydate%_%mytime%"
 call git fetch && git rebase origin/master
 call git push origin master
 popd
+
+::import all the files
+FOR /f "tokens=*" %%F IN ('dir %DIR%..\ /b *.ketarin.xml') DO (
+	call ketarin.exe /database="%DIR%\jobs.db" /import="%DIR%..\%%F"
+	TIMEOUT /T 2
+)
+
+Echo Wait for all of those to finish importing...
+TIMEOUT /T 15
 
 echo Calling ketarin now the the repo has been updated
 call "Ketarin.exe" /silent /notify /database="%DIR%\jobs.db" /log=C:\ProgramData\chocolateypackageupdater\ketarin.%mydate%_%mytime%.log
