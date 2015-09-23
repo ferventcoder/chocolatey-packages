da34fdbc31c128973edc1bd5e05f2cb6506171cb
Chocolatey <chocolatey@realdimensions.net>
2015-09-22 20:12:51 -0700

    updates prior to automatic run for 20150922_2012

---
+++ automatic/_output/nodejs.install/4.1.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'nodejs.install'
+$installerType = 'msi'
+$url = 'https://nodejs.org/dist/v4.1.1/node-v4.1.1-x86.msi'
+$url64 = 'https://nodejs.org/dist/v4.1.1/node-v4.1.1-x64.msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+
+$nodePath = "$env:SystemDrive\Program Files\nodejs"
+if (![System.IO.Directory]::Exists($nodePath)) {$nodePath = "$env:SystemDrive\Program Files (x86)\nodejs";}
+
+$env:Path = "$($env:Path);$nodePath"
