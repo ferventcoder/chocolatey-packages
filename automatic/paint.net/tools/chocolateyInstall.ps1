18903c6519a53a15c6ed05422cdf34f176858de4
Link Satonaka <link.satonaka@outlook.com>
2015-09-30 16:42:58 -0400

    remove my workaround, the bug will be fixed eventually

--- automatic/paint.net/tools/chocolateyInstall.ps1
+++ automatic/paint.net/tools/chocolateyInstall.ps1
@@ -1,13 +1,11 @@
 ﻿$packageName = 'paint.net'
-$fileType = 'EXE'
+$installType = 'EXE'
 $silentArgs = '/auto DESKTOPSHORTCUT=0'
 $url = '{{DownloadUrl}}'
 
 # Zipped installer
-Get-ChocolateyWebFile $packageName "$ENV:Temp\paint.net.install.zip" $url
-Get-ChocolateyUnzip "$ENV:Temp\paint.net.install.zip" "$ENV:Temp\pdn"
-$installFile = Get-ChildItem -Path "$ENV:Temp\pdn" -Recurse -Include "*.exe" | Select-Object -First 1
+Get-ChocolateyWebFile $packageName "$ENV:Temp\chocolatey\$packageName\paint.net.install.zip" $url
+Get-ChocolateyUnzip "$ENV:Temp\chocolatey\$packageName\paint.net.install.zip" "$ENV:Temp\chocolatey\$packageName"
+$file = Get-ChildItem -Path "$ENV:Temp\chocolatey\$packageName\" -Recurse -Include "*.exe" | Select-Object -First 1
 
-# elevation bug
-#Install-ChocolateyPackage $packageName $fileType $silentArgs $installFile
-Start-Process -FilePath "$installFile" -ArgumentList "$silentArgs" -Verb Runas
+Install-ChocolateyInstallPackage $packageName $installType $silentArgs $file
