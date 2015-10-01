c8065b322f1e78a8d37ef37e54e441de092c023a
Chocolatey <chocolatey@realdimensions.net>
2015-10-01 10:12:52 -0700

    updates prior to automatic run for 20151001_1012

---
+++ automatic/_output/7zip.commandline/15.08/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$versionMinusDots = "15.08".Replace(".","")
+
+$packageName = '7zip.commandline'
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$installDir = Join-Path "$toolsDir" '7zip'
+$fileName = "$($packageName).exe"
+$file = Join-Path "$toolsDir" "$fileName"
+
+$url = "http://www.7-zip.org/a/7z$($versionMinusDots).exe"
+$url64 = "http://www.7-zip.org/a/7z$($versionMinusDots)-x64.exe"
+
+Get-ChocolateyWebFile "$packageName" "$file" "$url" "$url64"
+Start-Process $file -argumentlist "/S /D=$toolsDir" -Wait
+
+$extrasUrl = "http://www.7-zip.org/a/7z$($versionMinusDots)-extra.7z"
+Install-ChocolateyZipPackage $packageName $extrasUrl $installDir
+
+if (Get-ProcessorBits 32) {
+  # generate ignore for x64\7za.exe
+  New-Item "$installDir\x64\7za.exe.ignore" -Type file -Force | Out-Null
+} else {
+  #generate ignore for 7za.exe and let x64 version pick up and shim
+  New-Item "$installDir\7za.exe.ignore" -Type file -Force | Out-Null
+}
