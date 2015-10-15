6cdd744874a0c8117e8d39dc96911c5bf71b6c3a
Chocolatey <chocolatey@realdimensions.net>
2015-10-14 19:12:51 -0700

    updates prior to automatic run for 20151014_1912

---
+++ automatic/_output/jetty/9.3.5.v20151012/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'jetty'
+$downloadUrl = 'http://mirror.netcologne.de/eclipse/jetty/stable-9/dist/jetty-distribution-9.3.5.v20151012.zip'
+$downloadUrl -match "dist/(.*)\.zip"
+$fileName = $matches[1]
+$validExitCodes = @(0)
+$installDir = Join-Path $(Get-BinRoot) $packageName
+$installSubDir = Join-Path $installDir $fileName
+#Install
+Install-ChocolateyZipPackage "$packageName" "$downloadUrl" "$installDir"
+
+# Move from subdir (e.g. jetty-v43253.543.45) in zip file to install root (jetty). This to support uninstall
+mv -Force (Join-Path $installSubDir "*") $installDir
+rd $installSubDir
+
+# Report completed
+write-host ($packageName + " installed to ") -NoNewLine
+write-host -foregroundcolor Yellow  $installDir
