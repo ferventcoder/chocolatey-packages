c3d0ec0278194166b6c7ff15aeca5cf6f3a9c43a
Chocolatey <chocolatey@realdimensions.net>
2015-11-10 12:12:56 -0800

    updates prior to automatic run for 20151110_1212

---
+++ automatic/_output/virtualbox/5.0.10.104061/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'virtualbox'
+$tools="$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$tools\oracle.cer'"
+Install-ChocolateyPackage $packageName 'exe' '-s -l -msiparams REBOOT=ReallySuppress' 'http://download.virtualbox.org/virtualbox/5.0.10/VirtualBox-5.0.10-104061-Win.exe'
+
+
+$is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
+$programFiles = $env:programfiles
+if ($is64bit) {$programFiles = ${env:ProgramFiles(x86)}}
+$fsObject = New-Object -ComObject Scripting.FileSystemObject
+$programFiles = $fsObject.GetFolder("$programFiles").ShortPath
+
+Install-ChocolateyPath $(join-path $programFiles 'Oracle\VirtualBox')
+
