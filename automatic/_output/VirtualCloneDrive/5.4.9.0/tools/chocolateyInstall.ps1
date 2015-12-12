42d64ae2b9a64b30ed99d44746bc22908b2cff8f
Chocolatey <chocolatey@realdimensions.net>
2015-12-12 13:12:52 -0800

    updates prior to automatic run for 20151212_1312

---
+++ automatic/_output/VirtualCloneDrive/5.4.9.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageId = 'VirtualCloneDrive'
+$fileType = 'exe'
+$fileArgs = '/S /noreboot'
+$url = 'http://static.slysoft.com/SetupVirtualCloneDrive.exe'
+
+$addCertificate = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\addCertificate.ps1"
+Start-ChocolateyProcessAsAdmin "& `'$addCertificate`'"
+Install-ChocolateyPackage $packageId $fileType $fileArgs $url
