5cd560e90fca18f73972c7bae0735d51486cb8ad
Link <Link-Satonaka@users.noreply.github.com>
2015-12-30 21:03:58 -0500

    errant newline

--- automatic/sumatrapdf.install/tools/chocolateyInstall.ps1
+++ automatic/sumatrapdf.install/tools/chocolateyInstall.ps1
@@ -5,4 +5,3 @@ $version = '{{PackageVersion}}'
 $url = "https://kjkpub.s3.amazonaws.com/sumatrapdf/rel/SumatraPDF-$version-install.exe"
 $url64 = "https://kjkpub.s3.amazonaws.com/sumatrapdf/rel/SumatraPDF-$version-64-install.exe"
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64 -validExitCodes @(0)
-
