50d51cc0d0c67f2692943d432b694ae1885b1509
Chocolatey <chocolatey@realdimensions.net>
2016-01-06 07:12:52 -0800

    updates prior to automatic run for 20160106_ 712

---
+++ automatic/_output/sqlite.shell/3.10.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'sqlite.shell'
+$url = 'https://www.sqlite.org/2016/sqlite-shell-win32-x86-3100000.zip'
+$PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+Install-ChocolateyZipPackage $packageName $url $PSScriptRoot
