4468562ccd7efe2cc98ef8013abe335ed28d9788
Rob Reynolds <ferventcoder@gmail.com>
2016-01-13 17:40:20 -0600

    update synergy package

--- manual/synergy/tools/chocolateyInstall.ps1
+++ manual/synergy/tools/chocolateyInstall.ps1
@@ -1,10 +1,22 @@
-﻿$packageName = 'synergy'
-$packageVersion = '1.7.5'
-$installerType = 'msi'
+﻿$packageName= 'synergy'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url   = "http://synergy-project.org/files/packages/synergy-v1.7.5-stable-fa85a24-Windows-x86.msi"
 $url64 = "http://synergy-project.org/files/packages/synergy-v1.7.5-stable-fa85a24-Windows-x64.msi"
-$silentArgs = '/quiet /norestart'
-$validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+  url64bit      = $url64
+  silentArgs    = "/qn /norestart"
+  validExitCodes= @(0, 3010, 1641)
 
+  softwareName  = 'Synergy*'
+  checksum      = ''
+  checksumType  = 'md5' #default is md5, can also be sha1
+  checksum64    = ''
+  checksumType64= 'md5' #default is checksumType
+}
+
+Install-ChocolateyPackage @packageArgs
