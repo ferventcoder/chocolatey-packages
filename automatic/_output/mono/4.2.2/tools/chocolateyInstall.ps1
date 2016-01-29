16af2479814329013b5b53c0280476e28a20839b
Chocolatey <chocolatey@realdimensions.net>
2016-01-29 07:13:00 -0800

    updates prior to automatic run for 20160129_ 712

---
+++ automatic/_output/mono/4.2.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = 'mono'
+$url = 'http://download.mono-project.com/archive/4.2.2/windows-installer/mono-4.2.2.30-gtksharp-2.12.30-win32-0.msi'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'msi' #only one of these: exe, msi, msu
+  url           = $url
+  silentArgs    = "/qn /norestart /l*v $env:TEMP\chocolatey\$packageName\install.log"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
