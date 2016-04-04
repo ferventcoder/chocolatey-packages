84d18d9c2955d16b5b3c46029178b0b226e37d22
Chocolatey <chocolatey@realdimensions.net>
2016-04-04 09:12:52 -0700

    updates prior to automatic run for 20160404_ 912

---
+++ automatic/_output/OptiPNG/0.7.6/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿try {
+  $package = 'OptiPNG'
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+  $zipUrl = 'http://sourceforge.net/projects/optipng/files/OptiPNG/optipng-0.7.6/optipng-0.7.6-win32.zip/download'
+
+  Install-ChocolateyZipPackage $package "$zipUrl" "$installDir"
+
+  Move-Item -Path "$($installDir)\optipng-0.7.6-win32\*" -Destination "$installDir" -Force
+  Remove-Item "$($installDir)\optipng-0.7.6-win32"
+
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
+
