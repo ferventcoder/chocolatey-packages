bcb1251011bde27d48d0f231016a317bbdac9c27
botmtl <botmtl@gmail.com>
2016-04-07 03:42:14 -0400

    Auto-Eula

--- automatic/sysinternals/tools/chocolateyInstall.ps1
+++ automatic/sysinternals/tools/chocolateyInstall.ps1
@@ -31,4 +31,9 @@ Install-ChocolateyZipPackage $packageName $url $installDir
   New-Item "$installDir\$_.exe.gui" -Type file -Force | Out-Null
 }
 
+New-Item "HKCU:\SOFTWARE\Sysinternals" -Force | Out-Null
+@("A","AccessChk","Active Directory Explorer","ADInsight","Autologon","AutoRuns","BGInfo","C","CacheSet","ClockRes","Coreinfo","Ctrl2cap","DbgView","Desktops","Disk2Vhd","Diskmon","DiskView","Du","EFSDump","FindLinks","Handle","Hex2Dec","Junction","LdmDump","ListDLLs","LoadOrder","Movefile","PageDefrag","PendMove","PipeList","Portmon","ProcDump","Process Explorer",  "Process Monitor","PsExec","psfile","PsGetSid","PsInfo","PsKill","PsList","PsLoggedon","PsLoglist","PsPasswd","PsService","PsShutdown","PsSuspend","RamMap","RegDelNull","Regjump","Regsize",  "RootkitRevealer","Share Enum","ShellRunas - Sysinternals: www.sysinternals.com","SigCheck","Streams","Strings","Sync","System Monitor","TCPView","VMMap","VolumeID","Whois","Winobj","ZoomIt") | % {
+  New-Item (Join-Path -Path 'HKCU:\SOFTWARE\Sysinternals' -ChildPath "$_") -Force | New-ItemProperty -Name "EulaAccepted" -Value 1 -Force | Out-Null
+  }
+
 Write-Warning "Clean up older versions of this install, most likely at c:\sysinternals"
