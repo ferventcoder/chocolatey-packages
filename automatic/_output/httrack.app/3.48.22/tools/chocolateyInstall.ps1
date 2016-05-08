3687ada6e090b3838547d0a1e680808cea2d8954
Chocolatey <chocolatey@realdimensions.net>
2016-05-08 06:12:52 -0700

    updates prior to automatic run for 20160508_ 612

---
+++ automatic/_output/httrack.app/3.48.22/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage 'httrack' 'exe' ' /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 'http://download.httrack.com/httrack-3.48.22.exe' 'http://download.httrack.com/httrack_x64-3.48.22.exe'  -validExitCodes @(0)
---
+++ automatic/_output/vlc/2.2.3/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'vlc'
+$fileType = 'exe'
+$silentArgs = '/S'
+$url = 'https://get.videolan.org/vlc/2.2.3/win32/vlc-2.2.3-win32.exe'
+$url64 = 'https://get.videolan.org/vlc/2.2.3/win64/vlc-2.2.3-win64.exe'
+
+Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url" "$url64"
