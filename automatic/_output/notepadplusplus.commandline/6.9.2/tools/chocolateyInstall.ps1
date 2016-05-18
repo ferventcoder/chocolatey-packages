a9fc84452879082dbfde48b4b527ec752b5ae079
Chocolatey <chocolatey@realdimensions.net>
2016-05-17 18:12:52 -0700

    updates prior to automatic run for 20160517_1812

---
+++ automatic/_output/notepadplusplus.commandline/6.9.2/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyZipPackage 'notepadplusplus.commandline' 'https://notepad-plus-plus.org/repository/6.x/6.9.2/npp.6.9.2.bin.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
