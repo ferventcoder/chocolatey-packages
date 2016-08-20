ff8af498bb2383ceeb1bdb6a7f0c5f65a9db2218
Rob Reynolds <ferventcoder@gmail.com>
2016-08-19 21:26:07 -0500

    add template packages

--- manual/default.template/templates/tools/chocolateyuninstall.ps1
+++ manual/default.template/templates/tools/chocolateyuninstall.ps1
@@ -2,9 +2,10 @@
 #   $f='c:\path\to\thisFile.ps1'
 #   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
 
-# If this is an MSI, cleaning up comments is all you need.
-# If this is an exe, change installerType and silentArgs
-# Auto Uninstaller should be able to detect and handle registry uninstalls (if it is turned on, it is in preview for 0.9.9).
+## If this is an MSI, cleaning up comments is all you need.
+## If this is an exe, change installerType and silentArgs
+## Auto Uninstaller should be able to detect and handle registry uninstalls (if it is turned on, it is in preview for 0.9.9).
+## - https://chocolatey.org/docs/helpers-uninstall-chocolatey-package
 
 $ErrorActionPreference = 'Stop'; # stop on all errors
 
@@ -34,13 +35,12 @@ if ($installerType -ne 'MSI') {
 }
 
 $uninstalled = $false
-$local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
-$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
-$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
-
-$key = Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) `
-                        -ErrorAction SilentlyContinue `
-         | ? { $_.DisplayName -like "$softwareName" }
+# Get-UninstallRegistryKey is new to 0.9.10, if supporting 0.9.9.x and below,
+# take a dependency on "chocolatey-uninstall.extension" in your nuspec file.
+# This is only a fuzzy search if $softwareName includes '*'. Otherwise it is 
+# exact. In the case of versions in key names, we recommend removing the version
+# and using '*'.
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
 
 if ($key.Count -eq 1) {
   $key | % { 
@@ -75,8 +75,9 @@ if ($key.Count -eq 1) {
 
 
 ## OTHER HELPERS
-## https://github.com/chocolatey/choco/wiki/HelpersReference
-#Uninstall-ChocolateyZipPackage $packageName
-#Uninstall-BinFile # Only needed if you added one in the installer script, choco will remove the ones it added automatically.
-#remove any shortcuts you added
+## https://chocolatey.org/docs/helpers-reference
+#Uninstall-ChocolateyZipPackage $packageName # Only necessary if you did not unpack to package directory - see https://chocolatey.org/docs/helpers-uninstall-chocolatey-zip-package
+#Uninstall-ChocolateyEnvironmentVariable # 0.9.10+ - https://chocolatey.org/docs/helpers-uninstall-chocolatey-environment-variable 
+#Uninstall-BinFile # Only needed if you used Install-BinFile - see https://chocolatey.org/docs/helpers-uninstall-bin-file
+## Remove any shortcuts you added
 
