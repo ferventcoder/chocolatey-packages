a0da9cd7e1b8965b057eb3a45a45b39356cb0cdd
Link <link.satonaka@outlook.com>
2016-09-07 13:59:39 -0400

    scrape source site, not filehippo

--- automatic/foobar2000/tools/chocolateyInstall.ps1
+++ automatic/foobar2000/tools/chocolateyInstall.ps1
@@ -1,31 +1,9 @@
 ﻿$packageName = "foobar2000"
-$fileType = "exe"
+$installerType = "exe"
 $silentArgs = "/S"
-$pwd = "$(split-path -parent $MyInvocation.MyCommand.Definition)"
+$version = '{{PackageVersion}}'
+$tempURL = (((invoke-webrequest -uri "https://foobar2000.org/download" -usebasicparsing).Links.Href | Select-String -Pattern .*foobar2000_v${version}.exe).Line).Replace("getfile","files")
+$url = "https://foobar2000.org$tempURL"
 
-Import-Module "$($pwd)\Get-FilenameFromRegex.ps1" -Force
-# Why does an import failure on this module not throw an error?
-
-$ErrorActionPreference = "Stop"
-
-try {
-  $url1 = Get-FilenameFromRegex "http://www.filehippo.com/download_foobar2000/history/" 'download_foobar2000/([\d]+)/"[^>]*>Foobar2000 {{PackageVersion}}<' 'http://www.filehippo.com/download_foobar2000/$1/'
-} catch {
-  Write-Host "Looking for alternative url"
-  $latestVersion = Get-FilenameFromRegex "http://www.filehippo.com/download_foobar2000/" 'itemprop\=\"name\"\>[\s\r\n]*Foobar2000 ({{PackageVersion}})<' '$1'
-  if ($latestVersion -ne '') {
-    Write-Host "Attempting to find url for latest version"
-    $url1 = Get-FilenameFromRegex "http://www.filehippo.com/download_foobar2000/" 'download_foobar2000/download/([\w\d]+)/[^>]*>[\s\r\n]*<span>Download Latest Version<' 'http://www.filehippo.com/en/download_foobar2000/download/$1/'
-  }
-}
-
-Write-Host "Found URL that redirects to the download url: $url1"
-
-$url2 = Get-FilenameFromRegex "$url1" 'download_foobar2000/download/([\w\d]+)/' 'http://www.filehippo.com/en/download_foobar2000/download/$1/'
-Write-Host "Found secondary url that redirects to the download url: $url2"
-
-$url3 = Get-FilenameFromRegex "$url2" '/download/file/([\w\d]+)/' 'http://www.filehippo.com/download/file/$1/'
-Write-Host "Found download URL: $url3"
-
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url3
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url
 
