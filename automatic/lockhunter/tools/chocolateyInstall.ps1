57ae867e92fcc8f8362d680a01ed77f405fb28af
sippi90 <sippi90@users.noreply.github.com>
2016-09-15 22:11:14 +0200

    Update chocolateyInstall.ps1
    added checksum

--- automatic/lockhunter/tools/chocolateyInstall.ps1
+++ automatic/lockhunter/tools/chocolateyInstall.ps1
@@ -1,2 +1,18 @@
-﻿Install-ChocolateyPackage '{{PackageName}}' 'exe' '/VERYSILENT' '{{DownloadUrl}}' '{{DownloadUrl}}'
+$ErrorActionPreference = 'Stop'
 
+$packageName = 'lockhunter'
+$url32       = 'http://lockhunter.com/exe/lockhuntersetup_3-1-1.exe'
+$checksum32  = 'ADEA78761F18BAAF938F9B5A18EE3B07CB6426F8ADFA234F715630741130EE7D'
+
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'exe'
+  url                    = $url32
+  silentArgs             = '/VERYSILENT'
+  checksum               = $checksum32
+  checksumType           = 'sha256'
+  validExitCodes         = @(0)
+  registryUninstallerKey = $packageName
+}
+
+Install-ChocolateyPackage @packageArgs
