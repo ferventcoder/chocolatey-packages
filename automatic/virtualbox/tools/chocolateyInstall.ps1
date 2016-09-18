b00f94822b976b32245ade056ca9202f31800b1c
Rob Reynolds <ferventcoder@gmail.com>
2016-09-18 11:59:15 -0500

    virtualbox fixes

--- automatic/virtualbox/tools/chocolateyInstall.ps1
+++ automatic/virtualbox/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'virtualbox'
 $tools="$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$tools\oracle.cer'"
-Install-ChocolateyPackage $packageName 'exe' '-s -l -msiparams REBOOT=ReallySuppress' '{{DownloadUrl}}'
-
+Install-ChocolateyPackage $packageName 'exe' '-s -l -msiparams REBOOT=ReallySuppress' '{{DownloadUrl}}' `
+-Checksum '{{Checksum}}' -ChecksumType 'sha256'
 
 $is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
 $programFiles = $env:programfiles
