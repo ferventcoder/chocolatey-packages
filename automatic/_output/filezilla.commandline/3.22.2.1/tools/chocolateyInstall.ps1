c13dc8a675674bd89de7ead1a72d4e23914166bd
Chocolatey <chocolatey@realdimensions.net>
2016-11-01 10:12:52 -0700

    updates prior to automatic run for 20161101_1012

---
+++ automatic/_output/filezilla.commandline/3.22.2.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'filezilla.commandline'
+$url = 'https://sourceforge.net/projects/filezilla/files/FileZilla_Client/3.22.2.1/FileZilla_3.22.2.1_win32.zip/download'
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# Rename folder, otherwise the *.exe.gui and *.exe.ignore files wouldn’t have effect
+Rename-Item -Path "$unzipLocation\FileZilla" -NewName 'FileZilla-3.22.2.1'
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
