3563dfdf27f087cf0a742b65f8ef7fe4d5329ab0
Chocolatey <chocolatey@realdimensions.net>
2016-11-09 16:12:52 -0800

    updates prior to automatic run for 20161109_1612

---
+++ automatic/_output/jruby/9.1.6.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'jruby'
+$installerType = 'exe'
+$url = 'https://jruby.org.s3.amazonaws.com/downloads/9.1.6.0/jruby_windows_9_1_6_0.exe'
+$url64 = 'https://jruby.org.s3.amazonaws.com/downloads/9.1.6.0/jruby_windows_x64_9_1_6_0.exe'
+$silentArgs = '-q'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
