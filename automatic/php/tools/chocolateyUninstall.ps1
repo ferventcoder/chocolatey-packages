a40a6b16d0eb6d89ba53612d15e765eac23629f5
Gary Ewan Park <gep13@gep13.co.uk>
2016-11-28 13:03:17 +0000

    (php) Package has been migrated to Core Team Repo
    - source files removed
    - ketarin file removed
    - _output folder removed

--- automatic/php/tools/chocolateyUninstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿write-host 'Please make sure you have CGI installed in IIS'
-$packageName = '{{PackageName}}'
-$validExitCodes = @(0)
-$targetFolder = Join-Path $(Get-BinRoot) $packageName
-Echo ("Deleting " + $targetFolder)
-Remove-Item -Force -Recurse $targetFolder
