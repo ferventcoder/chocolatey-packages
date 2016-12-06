4653aad8c0d71497610da89af4e6c1fcc1f1b352
Boyan Kostadinov <boyank@gmail.com>
2016-12-06 13:22:32 -0500

    Working Spotify package

--- manual/spotify/tools/chocolateyInstall.ps1
+++ manual/spotify/tools/chocolateyInstall.ps1
@@ -1,21 +1,34 @@
-﻿try{
-  $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-
-  #Changing compat mode to avoid dialog stating that installer must run as a non admin user
-  $env:__COMPAT_LAYER="WINXPSP3"
-  Install-ChocolateyPackage 'spotify' 'exe' '/SILENT' 'https://download.spotify.com/SpotifySetup.exe'
-  $env:__COMPAT_LAYER=$null
-
-  $installerFile = Join-Path $scriptDir 'install.au3'
-  write-host "Finishing spotify install with AutoIt3 using `'$installerFile`'"
-  $installArgs = "/c autoit3 `"$installerFile`""
-  Start-Process "cmd" -ArgumentList "$installArgs" -Wait
-
-  Write-ChocolateySuccess 'spotify'
-} catch {
-  Write-ChocolateyFailure 'spotify' "$($_.Exception.Message)"
-  throw
+$script           = $MyInvocation.MyCommand.Definition
+$packageArgs      = @{
+  packageName     = 'Spotify'
+  fileType        = 'exe'
+  fileFullPath    = Join-Path (Split-Path -Parent $script) 'SpotifyFullSetup.exe'
+  url             = 'http://download.spotify.com/SpotifyFullSetup.exe'
+  softwareName    = 'Spotify*'
+  checksum        = 'DC1770943FCFADC07DF5E399BA89D071E1E82311284860573C08E05D31BBF965'
+  checksumType    = 'sha256'
+  silentArgs      = '/silent'
+  validExitCodes  = @(0, 3010, 1641)
 }
-finally {
-  $env:__COMPAT_LAYER=$null
-}
+
+# Get the Spotify installer
+Get-ChocolateyWebFile @packageArgs 
+
+# Start the installer as a scheduled task to get around the 'can't install as admin' issue
+$action = New-ScheduledTaskAction -Execute $packageArgs['fileFullPath'] -Argument $packageArgs['silentArgs']
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date)
+Register-ScheduledTask -TaskName $packageArgs['packageName'] -Action $action -Trigger $trigger
+Start-ScheduledTask -TaskName $packageArgs['packageName']
+Start-Sleep -s 1
+Unregister-ScheduledTask -TaskName $packageArgs['packageName'] -Confirm:$false
+
+# Wait for Spotify to start, and kill it
+$done = $false
+do {
+  if (Get-Process Spotify -ErrorAction SilentlyContinue) {
+    Stop-Process -name Spotify
+    $done = $true
+  }
+
+  Start-Sleep -s 10
+} until ($done)
--- manual/spotify/tools/install.au3
+++
@@ -1,8 +0,0 @@
-;WinWait("Spotify", "Spotify", 120)
-ProcessClose("spotify.exe")
-;If ControlClick("Exit Dropbox", "Are you sure", "[CLASS:Button; INSTANCE:1]")=0 Then
-;	MsgBox(0, "Autoit Dropbox install script", "Something's wrong: The ControlClick used to confirm the ;'are you sure' dialog returned failure - proceeding with file copy", 5)
-;EndIf
- 
-;Installation complete
-Exit
