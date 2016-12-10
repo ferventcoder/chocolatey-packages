98b051f5f379294f29eeeb5be2a5785fddbbbd12
Rob Reynolds <ferventcoder@gmail.com>
2016-12-10 16:03:48 -0600

    windirstat - 1.1.2 permanent fixes

--- manual/windirstat/tools/chocolateyInstall.ps1
+++ manual/windirstat/tools/chocolateyInstall.ps1
@@ -1,11 +1,18 @@
-﻿$packageName = 'windirstat'
-$fileType = 'exe'
-#$url = 'https://prdownloads.sourceforge.net/windirstat/windirstat1_1_2_setup.exe'
-$url = 'http://download01.windirstat.info/wds_current_setup.exe'
-#$url = 'https://sourceforge.net/projects/windirstat/files/windirstat/1.1.2%20installer%20re-release%20%28more%20languages%21%29/windirstat1_1_2_setup.exe/download'
-$silentArgs = '/S'
-$checksum = '6fa92dd2ca691c11dfbfc0a239e34369897a7fab'
+﻿$ErrorActionPreference = 'Stop';
 
+$packageName  = 'WinDirStat'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'windirstat1_1_2_setup.exe'
 
-Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url" -checksum $checksum -checksumType 'sha1'
+$packageArgs = @{
+  packageName   = $packageName
+  softwareName  = 'WinDirStat*'
+  file          = $fileLocation
+  fileType      = 'exe'
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+  checksum      = '370A27A30EE57247FADDEB1F99A83933247E07C8760A07ED82E451E1CB5E5CDD'
+  checksumType  = 'sha256'
+}
 
+Install-ChocolateyInstallPackage @packageArgs
