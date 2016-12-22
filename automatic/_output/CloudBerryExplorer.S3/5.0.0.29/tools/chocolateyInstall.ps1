b264583cdb2073be16a2e1e39ff964472257e6d9
Chocolatey <chocolatey@realdimensions.net>
2016-12-22 07:12:52 -0800

    updates prior to automatic run for 20161222_ 712

---
+++ automatic/_output/CloudBerryExplorer.S3/5.0.0.29/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'CloudBerryExplorer.S3'
+$installerType = 'exe'
+$url = 'http://www.cloudberrylab.com/download/CloudBerryExplorerSetup_v5.0.0.29_netv4.0.exe'
+$url64 = $url
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
