5c525ad5b76f98c66f6f10c96c810dbda2603bf7
Chocolatey <chocolatey@realdimensions.net>
2016-12-21 16:12:52 -0800

    updates prior to automatic run for 20161221_1612

---
+++ automatic/_output/dropbox/16.4.30/tools/functions.ps1
@@ -0,0 +1,6 @@
+﻿Function getDropboxRegProps() {
+ 
+  [array]$props = Get-UninstallRegistryKey -SoftwareName "Dropbox" # This will use Chocolatey Helper Get-UninstallRegistryKey to get all registry keys for Dropbox
+
+  return $props
+}
