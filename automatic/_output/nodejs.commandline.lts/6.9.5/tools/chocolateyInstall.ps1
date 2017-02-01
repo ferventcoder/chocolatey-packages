9e61f480efaf10c0a979e7368aef66294b0a288d
Chocolatey <chocolatey@realdimensions.net>
2017-01-31 17:12:52 -0800

    updates prior to automatic run for 20170131_1712

---
+++ automatic/_output/nodejs.commandline.lts/6.9.5/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'nodejs'
+$url = 'https://nodejs.org/dist/v6.9.5/win-x86/node.exe'
+$url64 = 'https://nodejs.org/dist/v6.9.5/win-x64/node.exe'
+
+try {
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+  $nodePath = Join-Path $installDir 'node.exe'
+  Get-ChocolateyWebFile "$packageName" "$nodePath" "$url" "$url64"
+
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
