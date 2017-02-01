dc25519f5c26dd3ecdb54f838153e14754d9c539
Rob Reynolds <ferventcoder@gmail.com>
2017-01-31 22:07:46 -0600

    Add visualstudiocode.portable

---
+++ manual/visualstudiocode.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'visualstudiocode.portable'
+  url           = "https://go.microsoft.com/fwlink/?LinkID=623231"
+  checksum      = '672B200B79226A1A5CBF5C342C65A8CB052B72C2E9DA8F0BC41DD6FE4C6B0DB7'
+  checksumType  = 'sha256'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
