5f2080b1bc6a8c3ced45a804bcf605d531476f57
Chocolatey <chocolatey@realdimensions.net>
2017-02-14 12:12:53 -0800

    updates prior to automatic run for 20170214_1212

---
+++ automatic/_output/puppet-agent/1.9.1/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'puppet'
+$url = 'https://downloads.puppetlabs.com/windows/puppet-agent-1.9.1-x86.msi'
+$url64 = 'https://downloads.puppetlabs.com/windows/puppet-agent-1.9.1-x64.msi'
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+  silentArgs    = "/qn /norestart"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
