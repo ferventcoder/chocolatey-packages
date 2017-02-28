50b72c50e8ddd4ee3560e1bfc714b54faf4c5126
Chocolatey <chocolatey@realdimensions.net>
2017-02-27 16:12:58 -0800

    updates prior to automatic run for 20170227_1612

---
+++ automatic/_output/puppet-agent.portable/1.9.2/tools/chocolateyuninstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'SilentlyContinue';
+$packageName = 'puppet'
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$installDir = Join-Path "$toolsDir" 'puppet'
+
+# Remove the BATs from choco bin
+$binFilesDir = Join-Path "$installDir" 'SourceDir\Puppet Labs\Puppet\bin'
+Remove-BinFile -name 'puppet' -path "$binFilesDir\puppet.bat"
+Remove-BinFile -name 'facter' -path "$binFilesDir\facter.bat"
+Remove-BinFile -name 'hiera' -path "$binFilesDir\hiera.bat"
+Remove-BinFile -name 'mco' -path "$binFilesDir\mco.bat"
+
+Write-Warning "This package does not clean up ProgramData\PuppetLabs. You will need to do that manually."
