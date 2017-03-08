b564e6fb24e51812bd0a58258f741a74d88337cf
Chocolatey <chocolatey@realdimensions.net>
2017-03-08 15:12:52 -0800

    updates prior to automatic run for 20170308_1512

---
+++ automatic/_output/stexbar/1.9.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'StExBar'
+$url = 'https://sourceforge.net/projects/stefanstools/files/StExBar/StExBar-1.9.0.msi/download'
+$url64 = 'https://sourceforge.net/projects/stefanstools/files/StExBar/StExBar64-1.9.0.msi/download'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'msi'
+  url           = $url
+  url64bit      = $url64
+  silentArgs    = "/qn /norestart"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
