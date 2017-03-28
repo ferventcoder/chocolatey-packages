06dbb8769e9922e1a699016c1d2effea13c1325d
Boyan Kostadinov <bkostadinov@gocontec.com>
2017-03-28 16:57:57 -0400

    Removed Spotify package as it is part of the core-team packages repository

--- manual/spotify/tools/chocolateyunInstall.ps1
+++
@@ -1,6 +0,0 @@
-$packageName = "spotify"
-$fileType = "exe"
-$args = "--uninstall -s"
-$filePath = "$env:APPDATA\Spotify\Spotify.exe"
- 
-Uninstall-ChocolateyPackage $packageName $fileType $args $filePath
