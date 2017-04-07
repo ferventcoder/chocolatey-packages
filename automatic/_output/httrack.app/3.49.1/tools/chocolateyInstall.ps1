c2f3dd10c2fe94c14f40b8c6ed557e6801aecd56
Chocolatey <chocolatey@realdimensions.net>
2017-04-06 20:12:52 -0700

    updates prior to automatic run for 20170406_2012

---
+++ automatic/_output/httrack.app/3.49.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage 'httrack' 'exe' ' /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 'https://download.httrack.com/httrack-3.49.1.exe' 'https://download.httrack.com/httrack_x64-3.49.1.exe'  -validExitCodes @(0)
