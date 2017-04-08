31423ac26b78100ece9fcd0544a80492ae24c451
Chocolatey <chocolatey@realdimensions.net>
2017-04-08 12:12:53 -0700

    updates prior to automatic run for 20170408_1212

---
+++ automatic/_output/grepwin/1.6.17/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'grepwin'
+$installerType = 'msi'
+$installArgs = '/quiet'
+$url = 'https://sourceforge.net/projects/grepwin/files/1.6.17/grepWin-1.6.17.msi/download'
+$url64 = 'https://sourceforge.net/projects/grepwin/files/1.6.17/grepWin-1.6.17-x64.msi/download'
+
+Install-ChocolateyPackage $packageName $installerType $installArgs $url $url64
