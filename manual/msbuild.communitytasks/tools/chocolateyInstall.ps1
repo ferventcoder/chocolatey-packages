5e2b8cb9a617dd3a9a169ce38a2a1e15217865ea
Artem Zinenko <zinenkoartem@gmail.com>
2017-06-14 14:59:55 +0300

    Upgrade MSBuild Community Tasks package to latest release

--- manual/msbuild.communitytasks/tools/chocolateyInstall.ps1
+++ manual/msbuild.communitytasks/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyPackage 'msbuild.communitytasks' 'msi' '/quiet' 'https://github.com/loresoft/msbuildtasks/releases/download/1.5.0.196/MSBuild.Community.Tasks.v1.5.0.196.msi'
+﻿Install-ChocolateyPackage 'msbuild.communitytasks' 'msi' '/quiet' 'https://github.com/loresoft/msbuildtasks/releases/download/1.5.0.235/MSBuild.Community.Tasks.v1.5.0.235.msi'
