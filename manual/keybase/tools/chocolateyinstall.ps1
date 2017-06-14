18a79a5c63765bff1e9f9c6cf08c39236ff7c924
Rob Reynolds <ferventcoder@gmail.com>
2017-06-14 16:49:44 -0500

    (keybase) initial

---
+++ manual/keybase/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Keybase*'
+  fileType      = 'exe'
+  silentArgs    = "/install /quiet /norestart"
+  validExitCodes= @(0)
+  url           = "https://prerelease.keybase.io/keybase_setup_386.exe"
+  checksum      = '357506406C23C67DA6845893368237482641C0ED9201FD27845ECE205208C9C1'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
+
