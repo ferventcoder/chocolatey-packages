ca2d5c9cc31894d38ea723ee338cecd8f5837eb4
Chocolatey <chocolatey@realdimensions.net>
2017-06-28 13:12:52 -0700

    updates prior to automatic run for 20170628_1312

---
+++ automatic/_output/puppet/2.7.12/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'puppet'
+$url = 'https://downloads.puppetlabs.com/windows/puppet-2.7.12.msi'
+$url64 = 'https://downloads.puppetlabs.com/windows/puppet-2.7.12-x64.msi'
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+  silentArgs    = "/qn /norestart"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
