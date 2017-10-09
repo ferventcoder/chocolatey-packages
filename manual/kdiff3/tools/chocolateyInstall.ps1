729d9fc347cba75809664818468d20fc84303f0d
Jakub Bereżański <kuba@berezanscy.pl>
2017-10-09 14:37:18 +0200

    (kdiff3) remove obsolete code

--- manual/kdiff3/tools/chocolateyInstall.ps1
+++ manual/kdiff3/tools/chocolateyInstall.ps1
@@ -1,23 +1,15 @@
-﻿try {
+﻿$packageName = 'kdiff3'
+$fileType = 'exe'
+$silentArgs = '/S'
+$url = 'https://downloads.sourceforge.net/project/kdiff3/kdiff3/0.9.98/KDiff3-32bit-Setup_0.9.98-3.exe'
+$url64 = 'https://downloads.sourceforge.net/project/kdiff3/kdiff3/0.9.98/KDiff3-64bit-Setup_0.9.98-2.exe'
 
-  $packageName = 'kdiff3'
-  $fileType = 'exe'
-  $silentArgs = '/S'
-  $url = 'https://downloads.sourceforge.net/project/kdiff3/kdiff3/0.9.98/KDiff3-32bit-Setup_0.9.98-3.exe'
-  $url64 = 'https://downloads.sourceforge.net/project/kdiff3/kdiff3/0.9.98/KDiff3-64bit-Setup_0.9.98-2.exe'
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64 -validExitCodes @(0)
 
-  Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64 -validExitCodes @(0)
-
-  #------additional setup ----------------
-  #add it to the path
-  $programPath = "$env:SystemDrive\Program Files\kdiff3"
-  if (![System.IO.Directory]::Exists($programPath)) {
-    $programPath = "$env:SystemDrive\Program Files (x86)\kdiff3";
-  }
-  Install-ChocolateyPath $programPath
-
-  Write-ChocolateySuccess $packageName
-} catch {
-  Write-ChocolateyFailure $packageName $($_.Exception.Message)
-  throw
+#------additional setup ----------------
+#add it to the path
+$programPath = "$env:SystemDrive\Program Files\kdiff3"
+if (![System.IO.Directory]::Exists($programPath)) {
+$programPath = "$env:SystemDrive\Program Files (x86)\kdiff3";
 }
+Install-ChocolateyPath $programPath
