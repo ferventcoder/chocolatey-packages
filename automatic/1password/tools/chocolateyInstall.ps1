4a3406bbd2e1733aaaeee7d75c7b3c23d50013c4
AdmiringWorm <kim.nordmo@gmail.com>
2018-02-26 20:46:13 +0100

    (1password) Removing package

--- automatic/1password/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$packageId = '1password'
-$fileType = 'exe'
-$installArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$url = '{{DownloadUrl}}'
-
-Install-ChocolateyPackage $packageId $fileType $installArgs $url -validExitCodes @(0)
