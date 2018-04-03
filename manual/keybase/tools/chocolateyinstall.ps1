0629ad1bb6ae4caa8de6126471ae2e22b2bcc900
Gary Ewan Park <gep13@gep13.co.uk>
2018-04-03 19:51:37 +0100

    (keybase) Updated version number

--- manual/keybase/tools/chocolateyinstall.ps1
+++ manual/keybase/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   softwareName           = 'Keybase*'
   FileType               = 'exe'
   SilentArgs             = '/quiet'
-  url                    = 'https://prerelease.keybase.io/keybase_setup_386.exe'
-  checksum               = '89d876b3188b44b702e7e9cbcb0290b543ea1639d01a9cbd1d0328e479399a28'
+  url                    = 'https://prerelease.keybase.io/windows/Keybase_1.0.47-20180402191728%2B19ebebe.386.exe'
+  checksum               = '4163F6D6BAC8E58559E62240D61FEBD469E99D10A7E69D4CACEAB826EC824B3B'
   checksumType           = 'sha256'
   validExitCodes         = @(0, 3010)
 }
