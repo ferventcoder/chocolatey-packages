23fb96dbd6c12a98a06454eebf7915ae0b389480
Gary Ewan Park <gep13@gep13.co.uk>
2018-04-06 11:26:09 +0100

    (keybase) Updating to latest version

--- manual/keybase/tools/chocolateyinstall.ps1
+++ manual/keybase/tools/chocolateyinstall.ps1
@@ -1,12 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+# Currently, version number information is taking from here:
+# http://s3.amazonaws.com/prerelease.keybase.io/update-windows-prod-v2.json
+
 $packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
   softwareName           = 'Keybase*'
   FileType               = 'exe'
   SilentArgs             = '/quiet'
-  url                    = 'https://prerelease.keybase.io/windows/Keybase_1.0.47-20180402191728%2B19ebebe.386.exe'
-  checksum               = '4163F6D6BAC8E58559E62240D61FEBD469E99D10A7E69D4CACEAB826EC824B3B'
+  url                    = 'https://prerelease.keybase.io/windows/Keybase_1.0.47-20180404145203%2B0fe66b9.386.exe'
+  checksum               = '90E4AB31AB2A93BF4F3DFA90A89C24BD23ADFED331545C0606CE459A394FE3FF'
   checksumType           = 'sha256'
   validExitCodes         = @(0, 3010)
 }
