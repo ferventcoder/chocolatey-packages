7f28302a704e95715d5156599df063a44793e773
Gary Ewan Park <gep13@gep13.co.uk>
2018-06-05 14:22:39 +0100

    (adobereader) Removed package
    - This is now being maintained here:
    https://github.com/aronovgj/choco-auto

--- automatic/adobereader/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = '{{PackageName}}'
-$installerType = 'EXE'
-#Command Line Switches for the Bootstrap Web Installer: https://forums.adobe.com/message/3291894#3291894
-$silentArgs = '/sAll /msi /norestart /quiet ALLUSERS=1 EULA_ACCEPT=YES'
-$url = '{{DownloadUrl}}'
-$validExitCodes = @(0, 3010)
-
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
