2b1c06eaa33238eb65e778591f86434483ce6071
Gary Ewan Park <gep13@gep13.co.uk>
2018-06-05 14:12:48 +0100

    (postgres) Remove this package
    - This is now being handled elsewhere

--- automatic/postgresql.v8/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿try {
-  $toolsDir ="$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  Start-ChocolateyProcessAsAdmin "& $($toolsDir)\installpostgre.ps1"
-
-  Write-ChocolateySuccess 'postgresql'
-} catch {
-  Write-ChocolateyFailure 'postgresql' "$($_.Exception.Message)"
-  throw
-}
