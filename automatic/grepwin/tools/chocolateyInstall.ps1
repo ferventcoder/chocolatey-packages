df7da13840c05cc0ddf10fcf574d3217a23f3e47
Stefan Küng <stefankueng@gmail.com>
2018-06-17 15:41:33 +0200

    Remove the grepWin package. It's now maintained with grepWin itself.

--- automatic/grepwin/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'grepwin'
-$installerType = 'msi'
-$installArgs = '/quiet'
-$url = 'https://sourceforge.net/projects/grepwin/files/{{PackageVersion}}/grepWin-{{PackageVersion}}.msi/download'
-$url64 = 'https://sourceforge.net/projects/grepwin/files/{{PackageVersion}}/grepWin-{{PackageVersion}}-x64.msi/download'
-
-Install-ChocolateyPackage $packageName $installerType $installArgs $url $url64
