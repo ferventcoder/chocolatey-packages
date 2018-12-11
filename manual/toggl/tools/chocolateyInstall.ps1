82738c001b12f3188bdf0bd4028025b220d02233
Gary Ewan Park <gep13@gep13.co.uk>
2018-12-11 13:33:57 +0000

    (toggl) Updated with checksum

--- manual/toggl/tools/chocolateyInstall.ps1
+++ manual/toggl/tools/chocolateyInstall.ps1
@@ -1,1 +1,14 @@
-﻿Install-ChocolateyPackage 'toggl' 'exe' '/S' 'https://toggl.com/api/v8/installer?app=td&platform=windows&channel=stable'
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = 'https://toggl.com/api/v8/installer?app=td&platform=windows&channel=stable'
+  softwareName   = 'Toggl Desktop'
+  checksum       = 'BC911D72BFD0D330628F38D9D7DCEEE9ECFD1E98B7DF45BBCFAC89C8B836D792'
+  checksumType   = 'sha256'
+  silentArgs     = "/S"
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
