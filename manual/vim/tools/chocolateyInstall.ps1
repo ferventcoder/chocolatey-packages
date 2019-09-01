8f545e5193d6b2b00f30b33825817b1e5432a766
n3wjack <n3wjack@n3wjack.net>
2019-09-01 20:08:48 +0200

    Moved the vim package to the manual folder, upgrade to vim 8.1.

---
+++ manual/vim/tools/chocolateyInstall.ps1
@@ -0,0 +1,33 @@
+﻿try {
+  $packageName = 'vim'
+  $fileType = 'exe'
+  $args = '/S'
+
+  $url = "https://ftp.nluug.nl/pub/vim/pc/gvim81.exe"
+  Install-ChocolateyPackage $packageName $fileType $args $url
+
+  # Get installation directory from registry
+  $regPath = 'HKLM:\SOFTWARE\Vim\Gvim'
+
+  if (Test-Path $regPath) {
+    $regPathFound = $regPath
+  }
+
+  if ($regPathFound) {
+    # Add the installation directory to PATH
+    $installDir = Split-Path -Parent (
+      Get-ItemProperty $regPathFound 'Path'
+    ).Path
+
+    Write-Host 'Adding the vim installation directory to PATH …'
+
+    Write-Host "Installdir : $installDir"
+
+    Install-ChocolateyPath $installDir 'Machine'
+    $env:Path = "$($env:Path);$installDir"
+  }
+
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
