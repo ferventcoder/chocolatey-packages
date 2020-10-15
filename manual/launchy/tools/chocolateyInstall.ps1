d6d055e549e506c1ddbf76b3bf0c209682dfc5ed
virtualex <info@ithinkvirtual.com>
2020-10-15 15:44:23 -0400

    Updated to 2.6.0-beta2

--- manual/launchy/tools/chocolateyInstall.ps1
+++ manual/launchy/tools/chocolateyInstall.ps1
@@ -1,2 +1,22 @@
-﻿Install-ChocolateyPackage 'launchy' 'exe' '/VERYSILENT /NORESTART' 'https://www.launchy.net/downloads/win/Launchy2.5.exe' -checksum 'C67962F064924F3C7B95D69F88E745C0'
+﻿$ErrorActionPreference  = 'Stop';
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName            = 'launchy'
+$fileType               = 'exe'
+$url                    = 'http://www.launchy.net/downloads/win/LaunchySetup2.6B2.exe'
+$softwareName           = 'Launchy*'
+$checksum               = '49E6E7F0FDD9BD16E30D827AC421BD9AFD5DD281577A9B08E8A0C3F91BAD62A1'
+$checksumType           = 'sha256'
+$silentArgs             = '/VERYSILENT /NORESTART'
+$validExitCodes         = @(0, 3010, 1605, 1614, 1641)
 
+    $packageArgs = @{
+      packageName       = $env:ChocolateyPackageName
+      fileType          = $fileType
+      url               = $url
+      checksum          = $checksum
+      checksumType      = $checksumType
+      silentArgs        = $silentArgs
+      validExitCodes    = $validExitCodes  
+    }
+
+Install-ChocolateyPackage @packageArgs
