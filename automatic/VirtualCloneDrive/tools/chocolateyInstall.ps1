ae829ebf4859081235b7370cc459d71a805f5ac2
dgalbraith <dg@lbraith.io>
2021-04-25 14:23:59 +0100

    (virtualclonedrive) migrate maintenance to dgalbraith

--- automatic/VirtualCloneDrive/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageId = 'VirtualCloneDrive'
-$fileType = 'exe'
-$fileArgs = '/S /noreboot'
-$url = '{{DownloadUrl}}'
-
-$addCertificate = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\addCertificate.ps1"
-Start-ChocolateyProcessAsAdmin "& `'$addCertificate`'"
-Install-ChocolateyPackage $packageId $fileType $fileArgs $url
